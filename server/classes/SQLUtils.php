7c5460568af501344b0e7157e2b688aea5dfe74d
pepe_fabra_valverde <jofaval@outlook.com>
2020-03-11 09:12:52 +0100

    Added transactions to SQLUtils

--- server/classes/SQLUtils.php
+++ server/classes/SQLUtils.php
@@ -11,116 +11,157 @@ class SQLUtils
 
     public function query($table, $values = "*", $params = [])
     {
-        $queryString = "SELECT $values FROM $table";
+        try {
+            $this->$model->beginTransaction();
+            $queryString = "SELECT $values FROM $table";
 
-        if (count($params) > 0) {
-            $queryString .= " WHERE ";
-            foreach ($params as $key => $value) {
-                $queryString .= ":$key=$key";
+            if (count($params) > 0) {
+                $queryString .= " WHERE ";
+                foreach ($params as $key => $value) {
+                    $queryString .= ":$key=$key";
+                }
             }
+
+            $queryAction = $this->$model->$conexion->prepare($queryString);
+
+            foreach ($params as $key => $value) {
+                $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
+            }
+
+            if ($queryAction->execute()) {
+                return $queryAction->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            $this->$model->commit();
+        } catch (PDOException $ex) {
+            $this->$model->rollback();
         }
-
-        $queryAction = $this->$model->$conexion->prepare($queryString);
-
-        foreach ($params as $key => $value) {
-            $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
-        }
-
-        if ($queryAction->execute()) {
-            return $queryAction->fetchAll(PDO::FETCH_ASSOC);
-        }
-
         return false;
     }
 
     public function complexQuery($queryString, $params = [])
     {
-        $queryAction = $this->$model->$conexion->prepare($queryString);
+        try {
+            $this->$model->beginTransaction();
+            $queryAction = $this->$model->$conexion->prepare($queryString);
 
-        foreach ($params as $key => $value) {
-            $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
+            foreach ($params as $key => $value) {
+                $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
+            }
+
+            if ($queryAction->execute()) {
+                return $queryAction->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            $this->$model->commit();
+        } catch (PDOException $ex) {
+            $this->$model->rollback();
         }
-
-        if ($queryAction->execute()) {
-            return $queryAction->fetchAll(PDO::FETCH_ASSOC);
-        }
-
         return false;
     }
 
     public function update($table, $toModify, $identificationParams = [])
     {
-        $queryString = "UPDATE $table SET ";
+        try {
+            $this->$model->beginTransaction();
+            $queryString = "UPDATE $table SET ";
 
-        $paramKeyNames = [];
-        foreach ($toModify as $key => $value) {
-            $paramKeyNames = ":$key=$key";
-        }
-
-        $queryString .= $paramKeyNames . join(", ");
-        if (count($identificationParams) > 0) {
-            $queryString .= "WHERE ";
-            foreach ($identificationParams as $key => $value) {
-                $queryString .= ":$key=$key";
+            $paramKeyNames = [];
+            foreach ($toModify as $key => $value) {
+                $paramKeyNames = ":$key=$key";
             }
+
+            $queryString .= $paramKeyNames . join(", ");
+            if (count($identificationParams) > 0) {
+                $queryString .= "WHERE ";
+                foreach ($identificationParams as $key => $value) {
+                    $queryString .= ":$key=$key";
+                }
+            }
+
+            $queryAction = $this->$model->$conexion->prepare($queryString);
+
+            foreach ($toModify as $key => $value) {
+                $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
+            }
+
+            foreach ($identificationParams as $key => $value) {
+                $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
+            }
+
+            $result = $queryString->execute();
+            $this->$model->commit();
+            return $result;
+        } catch (PDOException $ex) {
+            $this->$model->rollback();
         }
-
-        $queryAction = $this->$model->$conexion->prepare($queryString);
-
-        foreach ($toModify as $key => $value) {
-            $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
-        }
-
-        foreach ($identificationParams as $key => $value) {
-            $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
-        }
-
-        return $queryString->execute();
     }
 
     public function delete($table, $identificationParams = [])
     {
-        $queryString = "DELETE FROM $table";
+        try {
+            $this->$model->beginTransaction();
+            $queryString = "DELETE FROM $table";
 
-        if (count($identificationParams) > 0) {
-            $queryString .= " WHERE ";
-            foreach ($identificationParams as $key => $value) {
-                $queryString .= ":$key=$key";
+            if (count($identificationParams) > 0) {
+                $queryString .= " WHERE ";
+                foreach ($identificationParams as $key => $value) {
+                    $queryString .= ":$key=$key";
+                }
             }
+
+            $queryAction = $this->$model->$conexion->prepare($queryString);
+
+            foreach ($identificationParams as $key => $value) {
+                $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
+            }
+
+            $result = $queryString->execute();
+            $this->$model->commit();
+            return $result;
+        } catch (PDOException $ex) {
+            $this->$model->rollback();
         }
-
-        $queryAction = $this->$model->$conexion->prepare($queryString);
-
-        foreach ($identificationParams as $key => $value) {
-            $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
-        }
-
-        return $queryString->execute();
     }
 
     public function enable($table, $enable = true, $identificationParams = [])
     {
-        return update($table, ["enabled" => $enable], $identificationParams);
+        try {
+            $this->$model->beginTransaction();
+
+            $result = update($table, ["enabled" => $enable], $identificationParams);
+            $this->$model->commit();
+            return $result;
+        } catch (PDOException $ex) {
+            $this->$model->rollback();
+        }
     }
 
     public function insert($table, $params = [])
     {
-        $queryString = "INSERT INTO $table ";
+        try {
+            $this->$model->beginTransaction();
+            $queryString = "INSERT INTO $table ";
 
-        $paramKeyNames = [];
-        foreach ($params as $key => $value) {
-            $paramKeyNames = $key;
+            $paramKeyNames = [];
+            foreach ($params as $key => $value) {
+                $paramKeyNames = $key;
+            }
+
+            $queryString .= "(" . $paramKeyNames . join(", ") . ")";
+            $queryString .= "VALUES (:" . $paramKeyNames . join(", :") . ")";
+
+            $queryAction = $this->$model->$conexion->prepare($queryString);
+
+            foreach ($params as $key => $value) {
+                $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
+            }
+
+            $result = $queryString->execute();
+            $this->$model->commit();
+            return $result;
+        } catch (PDOException $ex) {
+            $this->$model->rollback();
         }
-
-        $queryString .= "(" . $paramKeyNames . join(", ") . ")";
-        $queryString .= "VALUES (:" . $paramKeyNames . join(", :") . ")";
-
-        $queryAction = $this->$model->$conexion->prepare($queryString);
-
-        foreach ($params as $key => $value) {
-            $queryAction->bindValue(":$key", $value, PDO::PARAM_STR);
-        }
-
-        return $queryString->execute();
     }
 }
