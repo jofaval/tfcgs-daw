80416e3ab566b1ce0f3ba45d242bd6aa58560671
pepe_fabra_valverde <jofaval@outlook.com>
2020-03-12 14:51:35 +0100

    Added POPOController

---
+++ server/classes/POPOs/POPOcontroller.php
@@ -0,0 +1,453 @@
+<?php
+
+class POPOcontroller
+{
+
+	public function createClients()
+	{
+		$popoInstance = new Clients();
+
+		$popoInstance->create();
+	}
+
+	public function updateClients()
+	{
+		$popoInstance = new Clients();
+
+		$popoInstance->update();
+	}
+
+	public function queryClients()
+	{
+		$popoInstance = new Clients();
+
+		$popoInstance->query();
+	}
+
+	public function deleteClients()
+	{
+		$popoInstance = new Clients();
+
+		$popoInstance->delete();
+	}
+
+	public function createCollaborators()
+	{
+		$popoInstance = new Collaborators();
+
+		$popoInstance->create();
+	}
+
+	public function updateCollaborators()
+	{
+		$popoInstance = new Collaborators();
+
+		$popoInstance->update();
+	}
+
+	public function queryCollaborators()
+	{
+		$popoInstance = new Collaborators();
+
+		$popoInstance->query();
+	}
+
+	public function deleteCollaborators()
+	{
+		$popoInstance = new Collaborators();
+
+		$popoInstance->delete();
+	}
+
+	public function createGanttDiagramGroupTaskAssignation()
+	{
+		$popoInstance = new GanttDiagramGroupTaskAssignation();
+
+		$popoInstance->create();
+	}
+
+	public function updateGanttDiagramGroupTaskAssignation()
+	{
+		$popoInstance = new GanttDiagramGroupTaskAssignation();
+
+		$popoInstance->update();
+	}
+
+	public function queryGanttDiagramGroupTaskAssignation()
+	{
+		$popoInstance = new GanttDiagramGroupTaskAssignation();
+
+		$popoInstance->query();
+	}
+
+	public function deleteGanttDiagramGroupTaskAssignation()
+	{
+		$popoInstance = new GanttDiagramGroupTaskAssignation();
+
+		$popoInstance->delete();
+	}
+
+	public function createGanttDiagramGroupTasks()
+	{
+		$popoInstance = new GanttDiagramGroupTasks();
+
+		$popoInstance->create();
+	}
+
+	public function updateGanttDiagramGroupTasks()
+	{
+		$popoInstance = new GanttDiagramGroupTasks();
+
+		$popoInstance->update();
+	}
+
+	public function queryGanttDiagramGroupTasks()
+	{
+		$popoInstance = new GanttDiagramGroupTasks();
+
+		$popoInstance->query();
+	}
+
+	public function deleteGanttDiagramGroupTasks()
+	{
+		$popoInstance = new GanttDiagramGroupTasks();
+
+		$popoInstance->delete();
+	}
+
+	public function createGanttDiagramGroups()
+	{
+		$popoInstance = new GanttDiagramGroups();
+
+		$popoInstance->create();
+	}
+
+	public function updateGanttDiagramGroups()
+	{
+		$popoInstance = new GanttDiagramGroups();
+
+		$popoInstance->update();
+	}
+
+	public function queryGanttDiagramGroups()
+	{
+		$popoInstance = new GanttDiagramGroups();
+
+		$popoInstance->query();
+	}
+
+	public function deleteGanttDiagramGroups()
+	{
+		$popoInstance = new GanttDiagramGroups();
+
+		$popoInstance->delete();
+	}
+
+	public function createGanttDiagramStatus()
+	{
+		$popoInstance = new GanttDiagramStatus();
+
+		$popoInstance->create();
+	}
+
+	public function updateGanttDiagramStatus()
+	{
+		$popoInstance = new GanttDiagramStatus();
+
+		$popoInstance->update();
+	}
+
+	public function queryGanttDiagramStatus()
+	{
+		$popoInstance = new GanttDiagramStatus();
+
+		$popoInstance->query();
+	}
+
+	public function deleteGanttDiagramStatus()
+	{
+		$popoInstance = new GanttDiagramStatus();
+
+		$popoInstance->delete();
+	}
+
+	public function createGanttDiagrams()
+	{
+		$popoInstance = new GanttDiagrams();
+
+		$popoInstance->create();
+	}
+
+	public function updateGanttDiagrams()
+	{
+		$popoInstance = new GanttDiagrams();
+
+		$popoInstance->update();
+	}
+
+	public function queryGanttDiagrams()
+	{
+		$popoInstance = new GanttDiagrams();
+
+		$popoInstance->query();
+	}
+
+	public function deleteGanttDiagrams()
+	{
+		$popoInstance = new GanttDiagrams();
+
+		$popoInstance->delete();
+	}
+
+	public function createGlobalLevel()
+	{
+		$popoInstance = new GlobalLevel();
+
+		$popoInstance->create();
+	}
+
+	public function updateGlobalLevel()
+	{
+		$popoInstance = new GlobalLevel();
+
+		$popoInstance->update();
+	}
+
+	public function queryGlobalLevel()
+	{
+		$popoInstance = new GlobalLevel();
+
+		$popoInstance->query();
+	}
+
+	public function deleteGlobalLevel()
+	{
+		$popoInstance = new GlobalLevel();
+
+		$popoInstance->delete();
+	}
+
+	public function createPermissions()
+	{
+		$popoInstance = new Permissions();
+
+		$popoInstance->create();
+	}
+
+	public function updatePermissions()
+	{
+		$popoInstance = new Permissions();
+
+		$popoInstance->update();
+	}
+
+	public function queryPermissions()
+	{
+		$popoInstance = new Permissions();
+
+		$popoInstance->query();
+	}
+
+	public function deletePermissions()
+	{
+		$popoInstance = new Permissions();
+
+		$popoInstance->delete();
+	}
+
+	public function createProjectDiary()
+	{
+		$popoInstance = new ProjectDiary();
+
+		$popoInstance->create();
+	}
+
+	public function updateProjectDiary()
+	{
+		$popoInstance = new ProjectDiary();
+
+		$popoInstance->update();
+	}
+
+	public function queryProjectDiary()
+	{
+		$popoInstance = new ProjectDiary();
+
+		$popoInstance->query();
+	}
+
+	public function deleteProjectDiary()
+	{
+		$popoInstance = new ProjectDiary();
+
+		$popoInstance->delete();
+	}
+
+	public function createProjects()
+	{
+		$popoInstance = new Projects();
+
+		$popoInstance->create();
+	}
+
+	public function updateProjects()
+	{
+		$popoInstance = new Projects();
+
+		$popoInstance->update();
+	}
+
+	public function queryProjects()
+	{
+		$popoInstance = new Projects();
+
+		$popoInstance->query();
+	}
+
+	public function deleteProjects()
+	{
+		$popoInstance = new Projects();
+
+		$popoInstance->delete();
+	}
+
+	public function createTaskListItemListItems()
+	{
+		$popoInstance = new TaskListItemListItems();
+
+		$popoInstance->create();
+	}
+
+	public function updateTaskListItemListItems()
+	{
+		$popoInstance = new TaskListItemListItems();
+
+		$popoInstance->update();
+	}
+
+	public function queryTaskListItemListItems()
+	{
+		$popoInstance = new TaskListItemListItems();
+
+		$popoInstance->query();
+	}
+
+	public function deleteTaskListItemListItems()
+	{
+		$popoInstance = new TaskListItemListItems();
+
+		$popoInstance->delete();
+	}
+
+	public function createTaskListItemLists()
+	{
+		$popoInstance = new TaskListItemLists();
+
+		$popoInstance->create();
+	}
+
+	public function updateTaskListItemLists()
+	{
+		$popoInstance = new TaskListItemLists();
+
+		$popoInstance->update();
+	}
+
+	public function queryTaskListItemLists()
+	{
+		$popoInstance = new TaskListItemLists();
+
+		$popoInstance->query();
+	}
+
+	public function deleteTaskListItemLists()
+	{
+		$popoInstance = new TaskListItemLists();
+
+		$popoInstance->delete();
+	}
+
+	public function createTaskLists()
+	{
+		$popoInstance = new TaskLists();
+
+		$popoInstance->create();
+	}
+
+	public function updateTaskLists()
+	{
+		$popoInstance = new TaskLists();
+
+		$popoInstance->update();
+	}
+
+	public function queryTaskLists()
+	{
+		$popoInstance = new TaskLists();
+
+		$popoInstance->query();
+	}
+
+	public function deleteTaskLists()
+	{
+		$popoInstance = new TaskLists();
+
+		$popoInstance->delete();
+	}
+
+	public function createTaskListsOrderCriteria()
+	{
+		$popoInstance = new TaskListsOrderCriteria();
+
+		$popoInstance->create();
+	}
+
+	public function updateTaskListsOrderCriteria()
+	{
+		$popoInstance = new TaskListsOrderCriteria();
+
+		$popoInstance->update();
+	}
+
+	public function queryTaskListsOrderCriteria()
+	{
+		$popoInstance = new TaskListsOrderCriteria();
+
+		$popoInstance->query();
+	}
+
+	public function deleteTaskListsOrderCriteria()
+	{
+		$popoInstance = new TaskListsOrderCriteria();
+
+		$popoInstance->delete();
+	}
+
+	public function createUsers()
+	{
+		$popoInstance = new Users();
+
+		$popoInstance->create();
+	}
+
+	public function updateUsers()
+	{
+		$popoInstance = new Users();
+
+		$popoInstance->update();
+	}
+
+	public function queryUsers()
+	{
+		$popoInstance = new Users();
+
+		$popoInstance->query();
+	}
+
+	public function deleteUsers()
+	{
+		$popoInstance = new Users();
+
+		$popoInstance->delete();
+	}
+}
