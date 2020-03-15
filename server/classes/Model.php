157d4ee1c0fdafe335c8959983b159d91a9e1eee
pepe_fabra_valverde <jofaval@outlook.com>
2020-03-15 19:13:46 +0100

    Add signup function to Model

--- server/classes/Model.php
+++ server/classes/Model.php
@@ -14,6 +14,7 @@ class Model extends PDO
         $this->conexion = new PDO('mysql:host=' . Config::$mvc_bd_hostname . ';dbname=' . Config::$mvc_bd_nombre . '', Config::$mvc_bd_usuario, Config::$mvc_bd_clave);
         // Realiza el enlace con la BD en utf-8
         $this->conexion->exec("set names utf8");
+        $this->conexion->exec("SET auto_commit = 0");
         $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
@@ -21,6 +22,11 @@ class Model extends PDO
     {
         if (self::$instance == null) {
             self::$instance = new Model();
+            self::$instance->conexion = new PDO('mysql:host=' . Config::$mvc_bd_hostname . ';dbname=' . Config::$mvc_bd_nombre . '', Config::$mvc_bd_usuario, Config::$mvc_bd_clave);
+            // Realiza el enlace con la BD en utf-8
+            self::$instance->conexion->exec("set names utf8");
+            self::$instance->conexion->exec("SET auto_commit = 0");
+            self::$instance->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return self::$instance;
     }
@@ -59,6 +65,65 @@ class Model extends PDO
         $params = ["username" => $username];
         $signin = $this->query("SELECT type, password, image FROM users WHERE username=:username or email=:username", $params);
         return $signin;
+    }
+
+    public function signup($firstName, $secondName, $email, $username, $password)
+    {
+
+        $params = [
+            "email" => $email,
+        ];
+
+        //echo "Empieza la transaccion";
+        //$this->$conexion->beginTransaction();
+        try {
+            $queryString = 'SELECT *
+                        FROM `clients`
+                            WHERE `email`=:email';
+            $emails = $this->query($queryString, $params);
+
+            //echo "<pre>";
+            //var_dump($emails);
+            //echo "</pre>";
+            if (count($emails) != 1) {
+                $params["first_name"] = $firstName;
+                $params["second_name"] = $secondName;
+
+                $clientQueryString = 'INSERT Into `clients` (`first_name`, `second_name`, `email`)
+                Values (:first_name, :second_name, :email)';
+                $client = $this->cudOperation($clientQueryString, $params);
+
+                $queryString = 'SELECT `id`
+                FROM `clients`
+                    WHERE `email`=:email';
+                $clientId = $this->query($queryString, ["email" => $email])[0]["id"];
+
+                $params = [
+                    "username" => $username,
+                    "password" => Cryptography::blowfishCrypt($password, $username),
+                    "id" => $clientId,
+                ];
+
+                $userQueryString = 'INSERT Into `users` (`id`, `username`, `password`, `level`)
+                Values (:id, :username, :password, 1)';
+                $user = $this->cudOperation($userQueryString, $params);
+
+                //echo "Tiene exito";
+                //$this->$conexion->commit();
+                return $client && $user;
+            }
+
+            //echo "Ya existe ese cliente";
+            //$this->$conexion->rollBack();
+        } catch (\Throwable $th) {
+            //echo "<pre>";
+            //var_dump($th);
+            //echo "</pre>";
+            //echo "Ha surgido un error";
+            //$this->$conexion->rollBack();
+        }
+
+        return false;
     }
 
     public function getEventsFromMonth()
