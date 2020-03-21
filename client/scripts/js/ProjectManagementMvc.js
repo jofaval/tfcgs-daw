de00f656bf8e08bb393f5ff96aff8922ce26b6e7
pepe_fabra_valverde <jofaval@outlook.com>
2020-03-21 22:15:42 +0100

    Add ProjectManagementMvc base

---
+++ client/scripts/js/ProjectManagementMvc.js
@@ -0,0 +1,38 @@
+class Model {
+    constructor() {
+
+    }
+}
+
+class View {
+    constructor() {
+
+    }
+
+    hideComponent(component) {
+        //component.fadeOut();
+        component.hide();
+    }
+
+    showComponent(component) {
+        //component.fadeIn();
+        component.show();
+    }
+}
+
+class Controller {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+
+        var mainContainer = $("#content");
+
+        view.initializeView(mainContainer);
+    }
+
+}
+
+const projectsController = new Controller(
+    new Model(),
+    new View()
+);
