2274dffba615bb9f3cc346870d9cefc81a6c1d33
pepe_fabra_valverde <jofaval@outlook.com>
2020-03-22 15:41:46 +0100

    Cleaned error 404

--- server/templates/error404.php
+++ server/templates/error404.php
@@ -2,7 +2,7 @@
 <?php $optionalCSS = ["message.css", "signin.css"];?>
 <?php $optionalScripts = ["js/customCursor.js"];?>
 <?php $title = "404 Not found";?>
-<?php $mainClasses = "waves-effect waves-light rgba-white-slight";?>
+<?php $mainClasses = "";?>
 <?php $showFooter = true;?>
 <?php $showHeader = true;?>
 <?php $showBreadcrumb = false;?>
@@ -10,7 +10,7 @@
 
 <?php ob_start()?>
 
-<div class="container text-center" style="z-index: 50;">
+<div class="container text-center">
     <h1 class="font-weight-bold" style="font-size: 10em;">404</h1>
     <h1 style="font-size: 6em;">Mamma mia!</h1>
     <h2>We have not found what you requested!</h2>
