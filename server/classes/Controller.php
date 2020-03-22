567aa8e692f264b3eb66b9eb355aed1d7fa97de5
pepe_fabra_valverde <jofaval@outlook.com>
2020-03-22 20:10:47 +0100

    Add elements redirection

--- server/classes/Controller.php
+++ server/classes/Controller.php
@@ -40,16 +40,24 @@ class Controller
             "creation_date" => "",
         ];
         if (Utils::exists("id")) {
-            Utils::getCleanedData("id");
             $project = new Projects();
             $projectData = $project->query()[0];
             $viewParams["title"] = $projectData["title"];
             $viewParams["creation_date"] = $projectData["creation_date"];
+
+            $direction = "project";
+
+            if (Utils::exists("element")) {
+                $element = Utils::getCleanedData("element");
+                if (in_array($element, ["gantt", "tasks", "agenda"])) {
+                    $direction = $element;
+                }
+            }
+
+            require __DIR__ . "/../templates/$direction.php";
         } else {
             header("Location: /daw/projects/");
         }
-
-        require __DIR__ . '/../templates/project.php';
     }
 
     public function error404()
