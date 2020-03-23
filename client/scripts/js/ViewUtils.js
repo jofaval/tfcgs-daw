b05fc05477fd58aa671023e5694be00a1d182eb8
pepe_fabra_valverde <jofaval@outlook.com>
2020-03-23 18:47:50 +0100

    Add ViewUtils with form creation

---
+++ client/scripts/js/ViewUtils.js
@@ -0,0 +1,162 @@
+var $formBase = $(`
+<form action="" class="bg-white p-3 rounded shadow" method="POST">
+</form>
+`);
+
+var $formInput = $(`
+<div class="md-form col">
+    <input type="" class="form-control" />
+    <label for="" />
+</div>
+`);
+
+var $formSubmit = $(`
+<button class="btn btn-primary btn-block" type="submit"></button>
+`);
+
+var $formSelect = $(`
+<div class="md-form col">
+    <select class="browser-default custom-select"></select>
+</div>
+`);
+
+var $formOption = $(`
+<option></option>
+`);
+
+var $formTextArea = $(`
+<div class="md-form col">
+    <textarea class="md-textarea form-control"></textarea>
+    <label for="" />
+</div>
+`);
+
+var $formRow = $(`
+<div class="form-row">
+</div>`);
+
+var $formTitle = $(`
+<h1></h1>
+`);
+
+var $formCheckeable = $(`
+<div class="custom-control">
+    <input type="" class="custom-control-input">
+    <label class="custom-control-label"></label>
+</div>
+`);
+
+class ViewUtils {
+    static createForm(container, action = "", method = "POST") {
+        var clonedForm = $formBase.clone();
+
+        clonedForm.prop("action", action);
+        clonedForm.prop("method", method);
+        container.append(clonedForm);
+
+        return clonedForm;
+    }
+
+    static addInput(parent, id, type = "text", placeholder = "") {
+        var clonedInput = $formInput.clone();
+
+        var inputControl = clonedInput.find("input");
+        inputControl.prop("type", type);
+        inputControl.prop("placeholder", "");
+        inputControl.prop("id", id);
+        inputControl.prop("name", id);
+
+        var label = inputControl.next();
+        label.prop("for", id);
+        label.text(placeholder);
+
+        parent.append(clonedInput);
+
+        return clonedInput;
+    }
+
+    static addSelect(parent, id) {
+        var clonedInput = $formSelect.clone();
+
+        clonedInput.prop("id", id);
+        clonedInput.prop("name", id);
+
+        parent.append(clonedInput);
+
+        return clonedInput;
+    }
+
+    static addOptionToSelect(select, value, text) {
+        var clonedOption = $formOption.clone();
+
+        clonedOption.prop("value", value);
+        clonedOption.text(text);
+
+        select.find("select").append(clonedOption);
+
+        return clonedOption;
+    }
+
+    static addFormTitle(form, title) {
+        var clonedTitle = $formTitle.clone();
+
+        clonedTitle.text(title);
+        form.prepend(clonedTitle);
+
+        return clonedTitle;
+    }
+
+    static addTextArea(parent, id, placeholder = "") {
+        var clonedInput = $formTextArea.clone();
+
+        var inputControl = clonedInput.find("textarea");
+        inputControl.prop("placeholder", "");
+        inputControl.prop("id", id);
+        inputControl.prop("name", id);
+
+        var label = inputControl.next();
+        label.prop("for", id);
+        label.text(placeholder);
+
+        parent.append(clonedInput);
+
+        return clonedInput;
+    }
+
+    static addFormRow(form) {
+        var clonedRow = $formRow.clone();
+
+        form.append(clonedRow);
+
+        return clonedRow;
+    }
+
+    static addFormCheckboxRadio(parent, text, id, type = "checkbox") {
+        var checkeable = $formCheckeable.clone();
+
+        var input = checkeable.find("input");
+        input.prop("type", type);
+        input.prop("id", id);
+        input.prop("name", id);
+
+        var label = input.next();
+        label.prop("for", id);
+        label.text(text);
+
+        parent.append(checkeable);
+
+        return checkeable;
+    }
+
+    static addSubmit(form, text = "", id = "submit") {
+        var submitInput = $formSubmit.clone();
+
+        submitInput.prop("id", id);
+        submitInput.prop("name", id);
+        submitInput.text(text);
+
+        form.append(submitInput);
+
+        return submitInput;
+    }
+}
