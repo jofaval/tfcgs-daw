ddbd48fc13eaa3ee2ce49ba4a8807069573759bc
pepe_fabra_valverde <jofaval@outlook.com>
2020-03-27 23:24:28 +0100

    Comment project bug

--- server/classes/Controller.php
+++ server/classes/Controller.php
@@ -47,19 +47,19 @@ class Controller
         if (Utils::exists("id")) {
             $id = Utils::getCleanedData("id");
 
-            $sessions = Sessions::getInstance();
-            $projectDataFromSession = $sessions->getSession("projectData");
-
             $project = new Projects();
             $projectData = $project->query()[0];
 
+            /* $sessions = Sessions::getInstance();
+            $projectDataFromSession = $sessions->getSession("projectData");
+
             if ($projectDataFromSession != null) {
-                if ($projectDataFromSession->$id != $id) {
-                    $projectDataFromSession = new ProjectData($this, $projectData);
-                }
-            } else {
-                $projectDataFromSession = new ProjectData($this, $projectData);
+            if ($projectDataFromSession->$id != $id) {
+            $projectDataFromSession = new ProjectData($this, $projectData);
             }
+            } else {
+            $projectDataFromSession = new ProjectData($this, $projectData);
+            } */
 
             $viewParams["title"] = $projectData["title"];
             $viewParams["id"] = $projectData["id"];
