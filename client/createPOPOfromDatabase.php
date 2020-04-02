3d6bd14d69abee6dceaeca55e6fc277e30801638
pepe_fabra_valverde <jofaval@outlook.com>
2020-04-02 20:12:03 +0200

    Refactor POPO script

--- client/createPOPOfromDatabase.php
+++ client/createPOPOfromDatabase.php
@@ -7,92 +7,100 @@ $mysqlPassword = "";
 $mysqlHostName = "localhost";
 $DbName = "database_proyecto_daw_2020";
 
-//$sqlUtils->lastInsertId();
-function createPOPOfromDatabase($host, $user, $pass, $name, $showTableInfo = true)
+$settings = [
+    "showTableLog" => false,
+    "overrideRouteMap" => false,
+    "overrideAccess" => false,
+    "overrideAjaxControllerPHP" => false,
+    "overrideAjaxControllerJS" => false,
+];
+
+function createPOPOfromDatabase($host, $user, $pass, $name)
 {
+    //Me conecto a la base de datos
     $mysqli = new mysqli($host, $user, $pass, $name);
+    //Selecciono la base de datos
     $mysqli->select_db($name);
+    //Fijo los carácteres a utf8
     $mysqli->query("SET NAMES 'utf8'");
 
+    //Almaceno todas las tablas de la base de datos en la variable $target_tables
     $queryTables = $mysqli->query('SHOW TABLES');
     while ($row = $queryTables->fetch_row()) {
         $target_tables[] = $row[0];
     }
 
+    //Nombres de las funciones que se van a implementar
     $functionNames = ["create", "update", "query", "delete"];
 
+    //Creo las variables de contenido
     $filesContent = [];
     $methods = [];
     $methodParams = [];
+    //Inicializo la clase de controller
     $controller = "<?php\n\nclass POPOcontroller\n{";
+
+    //Recorro todas las tablas que se han recogido
     foreach ($target_tables as $table) {
         $content = "";
 
+        //cambio el nombre de la tabla a NomeclaturaCamello
         $tableAsClass = camelCase($table, true);
 
+        //Recogo la creacion de la tabla
         $res = $mysqli->query('SHOW CREATE TABLE ' . $table);
         $TableMLine = $res->fetch_row()[1];
         $splittedLine = explode("\n", $TableMLine);
+        //Elimino la primera y última línea del Create table (las líneas de "CREATE TABLE tabla (" y ");")
         unset($splittedLine[0]);
         unset($splittedLine[count($splittedLine)]);
 
+        //Recogo todos los campos y las claves primarias y ajenas de la tabla
         $primaryKeys = [];
         $tableKeys = [];
         $foreignKeys = [];
         loadKeys($splittedLine, $tableKeys, $foreignKeys, $primaryKeys);
         $everyKey = array_merge($primaryKeys, $tableKeys, $foreignKeys);
 
-        foreach ($functionNames as $functionName) {
-            $methods[] = $functionName . "" . $tableAsClass;
-            switch ($functionName) {
-                case 'create':
-                case 'update':
-                    $methodParams[] = $everyKey;
-                    break;
-                case 'query':
-                case 'delete':
-                    $methodParams[] = $primaryKeys;
-                    break;
-            }
-            $controller .= "\n\n    public function $functionName$tableAsClass()\n    {";
-            $controller .= "\n        \$popoInstance = new " . $tableAsClass . "();";
-            $controller .= "\n\n        return \$popoInstance->$functionName();\n    }";
+        //Implemento las funciones CRUD de la tabla para el POPO
+        createFunctionsOfTable($functionNames, $methods, $tableAsClass, $methodParams, $controller, $everyKey, $primaryKeys);
+
+        //Se muestra la información de la tabla
+        if ($settings["showTableLog"]) {
+            showTableLog($table, $splittedLine, $primaryKeys, $tableKeys, $foreignKeys);
         }
 
-        //Table value
-        if ($showTableInfo) {
-            echo "<h1>$table</h1>";
-            echo "<pre>";
-            var_dump($splittedLine);
-            echo "</pre>";
-
-            echo "<pre>";
-            echo "<h2>primaryKeys</h2>";
-            var_dump($primaryKeys);
-            echo "<h2>tableKeys</h2>";
-            var_dump($tableKeys);
-            echo "<h2>foreignKeys</h2>";
-            var_dump($foreignKeys);
-            echo "</pre>";
-        }
-
+        //Se crea la clase POPO
         $content .= "<?php\n\nclass " . $tableAsClass . " implements CRUD \n{\n    private \$table = \"$table\";\n\n";
+        //Se recogen las propiedades de la tabla para introducirlas en las clases
         createClassProperties($content, $primaryKeys, $tableKeys, $foreignKeys);
         $content .= "\n";
+        //Relleno los campos al inicializar la clase POPO
         $content .= "\npublic function __construct()
         {
             \$this->fill();
         }";
+        //Creo las respectivas funciones
         addFunctions($content, $primaryKeys, $tableKeys, $foreignKeys, $everyKey);
         $content .= "\n} \n\n\n";
+
+        //Añado como archivo a crear el contenido para guardarlo en un archivo con el nombre de la clase
         $filesContent[$tableAsClass] = $content;
     }
 
-    createPHPajaxController($methods, $methodParams);
-    createJSajaxController($methods, $methodParams);
+    //Se crea/sobreescribe el controlador de AJAX para PHP
+    if ($settings["overrideAjaxControllerPHP"]) {
+        createPHPajaxController($methods, $methodParams);
+    }
+
+    //Se crea/sobreescribe el controlador de AJAX para JS
+    if ($settings["overrideAjaxControllerJS"]) {
+        createJSajaxController($methods, $methodParams);
+    }
 
     $controller .= "\n}";
 
+    //Se ejecuta la creación de los archivos y el respectivo controlador de POPOs, dentro de un if para depurar más fácilmente
     if (true) {
         echo "<h1>Clases</h1>";
         echo "<pre>";
@@ -111,6 +119,46 @@ function createPOPOfromDatabase($host, $user, $pass, $name, $showTableInfo = tru
     }
 }
 
+//Crea las funciones para el controlador de POPO
+function createFunctionsOfTable($functionNames, &$methods, $tableAsClass, &$methodParams, &$controller, &$everyKey, &$primaryKeys)
+{
+    foreach ($functionNames as $functionName) {
+        $methods[] = $functionName . "" . $tableAsClass;
+        switch ($functionName) {
+            case 'create':
+            case 'update':
+                $methodParams[] = $everyKey;
+                break;
+            case 'query':
+            case 'delete':
+                $methodParams[] = $primaryKeys;
+                break;
+        }
+        $controller .= "\n\n    public function $functionName$tableAsClass()\n    {";
+        $controller .= "\n        \$popoInstance = new " . $tableAsClass . "();";
+        $controller .= "\n\n        return \$popoInstance->$functionName();\n    }";
+    }
+}
+
+//Muestra la información que se ha recogido de cada tabla de la base de datos
+function showTableLog($table, $splittedLine, $primaryKeys, $tableKeys, $foreignKeys)
+{
+    echo "<h1>$table</h1>";
+    echo "<pre>";
+    var_dump($splittedLine);
+    echo "</pre>";
+
+    echo "<pre>";
+    echo "<h2>primaryKeys</h2>";
+    var_dump($primaryKeys);
+    echo "<h2>tableKeys</h2>";
+    var_dump($tableKeys);
+    echo "<h2>foreignKeys</h2>";
+    var_dump($foreignKeys);
+    echo "</pre>";
+}
+
+//Crea el controlador de AJAX para JS
 function createJSajaxController($methods, $methodParams)
 {
     $jsAjaxController = "class AjaxController {";
@@ -134,6 +182,7 @@ function createJSajaxController($methods, $methodParams)
     writeToFile("/scripts/js/AjaxController.js", $jsAjaxController);
 }
 
+//Crea el controlador de AJAX para PHP
 function createPHPajaxController($methods, $methodParams)
 {
     $phpAjaxController = "<?php\nclass AjaxController\n{";
@@ -158,6 +207,7 @@ function createPHPajaxController($methods, $methodParams)
     writeToFile("/../server/classes/AjaxController.php", $phpAjaxController);
 }
 
+//Añade las propiedades de la clase POPO en un orden específico
 function createClassProperties(&$content, $primaryKeys, $tableKeys, $foreignKeys)
 {
     $content .= "    //Primary Keys";
@@ -176,6 +226,7 @@ function createClassProperties(&$content, $primaryKeys, $tableKeys, $foreignKeys
     }
 }
 
+//Extrae del script CREATE TABLE anterior todas las claves
 function loadKeys($splittedLine, &$tableKeys, &$foreignKeys, &$primaryKeys)
 {
     foreach ($splittedLine as $value) {
@@ -202,6 +253,7 @@ function loadKeys($splittedLine, &$tableKeys, &$foreignKeys, &$primaryKeys)
     $foreignKeys = array_diff($foreignKeys, $primaryKeys);
 }
 
+//Función para sobreescribir un fichero
 function writeToFile($file, $fileContent)
 {
     $fileWriter = fopen(__DIR__ . $file, "w+");
@@ -209,30 +261,34 @@ function writeToFile($file, $fileContent)
     fclose($fileWriter);
 }
 
+//Crea las rutas del mapeo y niveles de acceso
 function mapRoutes($methods)
 {
-    $mapFile = "<?php\n\n\$map = [";
-    $controllerMethods = get_class_methods('Controller');
-    foreach ($controllerMethods as $method) {
-        $mapFile .= "\n    '$method' => array('controller' => 'Controller', 'action' => '$method', 'access' => Config::\$ACCESS_LEVEL_GUEST),";
+    if ($settings["overrideRouteMap"]) {
+        $mapFile = "<?php\n\n\$map = [";
+        $controllerMethods = get_class_methods('Controller');
+        foreach ($controllerMethods as $method) {
+            $mapFile .= "\n    '$method' => array('controller' => 'Controller', 'action' => '$method', 'access' => Config::\$ACCESS_LEVEL_GUEST),";
+        }
+        foreach ($methods as $method) {
+            $mapFile .= "\n    '$method' => array('controller' => 'AjaxController', 'action' => '$method', 'access' => Config::\$ACCESS_LEVEL_GUEST),";
+        }
+        $mapFile .= "\n];";
+        writeToFile("/../server/RoutingMap.php", $mapFile);
     }
-    foreach ($methods as $method) {
-        $mapFile .= "\n    '$method' => array('controller' => 'AjaxController', 'action' => '$method', 'access' => Config::\$ACCESS_LEVEL_GUEST),";
-    }
-    $mapFile .= "\n];";
 
-    writeToFile("/../server/RoutingMap.php", $mapFile);
-
-    $everyRoute = array_merge($controllerMethods, $methods);
-    $accessFile = "<?php\n";
-    foreach ($everyRoute as $value) {
-        $accessFile .= "\n\$map['error']['access'] = Config::\$ACCESS_LEVEL_GUEST;";
+    if ($settings["overrideAccess"]) {
+        $everyRoute = array_merge($controllerMethods, $methods);
+        $accessFile = "<?php\n";
+        foreach ($everyRoute as $value) {
+            $accessFile .= "\n\$map['error']['access'] = Config::\$ACCESS_LEVEL_GUEST;";
+        }
+        writeToFile("/../server/Access.php", $accessFile);
     }
-    writeToFile("/../server/Access.php", $accessFile);
 
 }
 
-//Create
+//Create POPO
 function createFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $everyKey)
 {
     $content .= "\n    public function create()\n    {\n        \$sqlUtils = new SQLUtils(Model::getInstance());\n\n        \$params = [";
@@ -242,7 +298,7 @@ function createFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $ever
     $content .= "\n        ];\n\n        return \$sqlUtils->insert(\$params);\n    }";
 }
 
-//Update
+//Update POPO
 function updateFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $everyKey)
 {
     $content .= "\n\n    public function update()\n    {\n        \$sqlUtils = new SQLUtils(Model::getInstance());\n\n        \$toModify = [";
@@ -261,7 +317,7 @@ function updateFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $ever
     $content .= "\n        ];\n\n        return \$sqlUtils->update(\$this->\$table, \$toModify, \$identificationParams);\n    }";
 }
 
-//Delete
+//Delete POPO
 function deleteFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $everyKey)
 {
     $content .= "\n\n    public function delete()\n    {\n        \$sqlUtils = new SQLUtils(Model::getInstance());\n\n        \$params = [";
@@ -271,7 +327,7 @@ function deleteFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $ever
     $content .= "\n        ];\n\n        return \$sqlUtils->delete(\$this->\$table, \$params);\n    }";
 }
 
-//Query
+//Query POPO
 function queryFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $everyKey)
 {
     $content .= "\n\n    public function query()\n    {\n        \$sqlUtils = new SQLUtils(Model::getInstance());\n\n        \$params = [";
@@ -281,7 +337,7 @@ function queryFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $every
     $content .= "\n        ];\n\n        return \$sqlUtils->query(\$this->\$table, \$params);\n    }";
 }
 
-//Enable
+//Enable POPO
 function enableFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $everyKey)
 {
     $content .= "\n\n    public function enable()\n    {\n        \$sqlUtils = new SQLUtils(Model::getInstance());\n\n        \$identificationParams = [";
@@ -291,7 +347,7 @@ function enableFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $ever
     $content .= "\n        ];\n\n        return \$sqlUtils->enable(\$this->\$table, Utils::getCleanedData(\"enable\"), \$identificationParams);\n    }";
 }
 
-//Fill
+//Fill POPO
 function fillFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $everyKey)
 {
     $content .= "\n\n\n    public function fill()\n    {";
@@ -301,7 +357,7 @@ function fillFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $everyK
     $content .= "\n    }";
 }
 
-//Parse
+//Parse POPO
 function parseFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $everyKey)
 {
     $content .= "\n\n\n    public function parse()\n    {\n        return json_encode([";
@@ -311,6 +367,7 @@ function parseFunction(&$content, $primaryKeys, $tableKeys, $foreignKeys, $every
     $content .= "\n        ]);\n    }";
 }
 
+//Añadir las funciones anteriores de POPO
 function addFunctions(&$content, $primaryKeys, $tableKeys, $foreignKeys, $everyKey)
 {
     createFunction($content, $primaryKeys, $tableKeys, $foreignKeys, $everyKey);
@@ -322,6 +379,7 @@ function addFunctions(&$content, $primaryKeys, $tableKeys, $foreignKeys, $everyK
     parseFunction($content, $primaryKeys, $tableKeys, $foreignKeys, $everyKey);
 }
 
+//Pasar a camel case
 function camelCase($string = "", $firstLetterCapital = false)
 {
     if (!$string) {
@@ -346,6 +404,7 @@ function camelCase($string = "", $firstLetterCapital = false)
     return $parsedString;
 }
 ?>
+<!--El formulario para realizar las peticiones-->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -353,7 +412,7 @@ function camelCase($string = "", $firstLetterCapital = false)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Database Mapping</title>
     <link rel="stylesheet" href="./styles/bootstrap.min.css">
     <link rel="stylesheet" href="./styles/jquery-ui.min.css">
     <link rel="stylesheet" href="./styles/mdb.min.css">
@@ -371,7 +430,7 @@ function camelCase($string = "", $firstLetterCapital = false)
                 <h2>Crear <span class="font-weight-bold">P</span>lain <span class="font-weight-bold">O</span>ld <span
                         class="font-weight-bold">P</span>HP <span class="font-weight-bold">O</span>bject a partir de una
                     base de datos</h2>
-                <select class="custom-select " name="dbName" id="">
+                <select class="custom-select " name="dbName" id="dbName">
                     <?php
 $mysqli = new mysqli($mysqlHostName, $mysqlUserName, $mysqlPassword, $DbName);
 $queryDatabases = $mysqli->query("SHOW DATABASES");
@@ -383,8 +442,32 @@ while ($row = $queryDatabases->fetch_row()) {
 ?>
                 </select>
                 <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" id="showTableInfo" name="showTableInfo">
-                    <label class="custom-control-label" for="showTableInfo">Mostrar información de cada tabla</label>
+                    <input type="checkbox" class="custom-control-input" id="showTableLog" name="showTableLog" checked>
+                    <label class="custom-control-label" for="showTableLog">Mostrar información de cada tabla</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="overrideRouteMap" name="overrideRouteMap"
+                        checked>
+                    <label class="custom-control-label" for="overrideRouteMap">Sobreescribir el archivo de mapeo de
+                        rutas</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="overrideAccess" name="overrideAccess"
+                        checked>
+                    <label class="custom-control-label" for="overrideAccess">Sobreescribir el archivo de nivel de
+                        acceso</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="overrideAjaxControllerPHP" checked
+                        name="overrideAjaxControllerPHP">
+                    <label class="custom-control-label" for="overrideAjaxControllerPHP">Sobreescribir controlador de
+                        AJAX de PHP</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="overrideAjaxControllerJS" checked
+                        name="overrideAjaxControllerJS">
+                    <label class="custom-control-label" for="overrideAjaxControllerJS">Sobreescribir controlador de AJAX
+                        de JS</label>
                 </div>
                 <input type="submit" class="btn btn-primary mx-auto" name="createPopo" value="Create POPOs">
             </form>
@@ -394,11 +477,19 @@ while ($row = $queryDatabases->fetch_row()) {
             <?php
 if (isset($_REQUEST["createPopo"])) {
     $DbName = $_REQUEST["dbName"];
-    createPOPOfromDatabase($mysqlHostName, $mysqlUserName, $mysqlPassword, $DbName, isset($_REQUEST["showTableInfo"]));
+    $settings["showTableLog"] = isset($_REQUEST["showTableInfo"]);
+    $settings["overrideRouteMap"] = isset($_REQUEST["overrideRouteMap"]);
+    $settings["overrideAccess"] = isset($_REQUEST["overrideAccess"]);
+    $settings["overrideAjaxControllerPHP"] = isset($_REQUEST["overrideAjaxControllerPHP"]);
+    $settings["overrideAjaxControllerJS"] = isset($_REQUEST["overrideAjaxControllerJS"]);
+    createPOPOfromDatabase($mysqlHostName, $mysqlUserName, $mysqlPassword, $DbName);
 }
 ?>
         </div>
     </main>
+    <script>
+    document.querySelector("#dbName").value = "database_proyecto_daw_2020";
+    </script>
 </body>
 
 </html>
