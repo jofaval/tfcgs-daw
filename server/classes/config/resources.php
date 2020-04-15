8b3012183eda6ba0aa17b42624fc788c667a0d8e
pepe_fabra_valverde <jofaval@outlook.com>
2020-04-15 18:56:04 +0200

    Fixed resources linking

--- server/classes/config/resources.php
+++ server/classes/config/resources.php
@@ -1,30 +1,30 @@
 <?php
 
 //CSS
-$mvc_vis_css[] = "bootstrap.min.css";
-$mvc_vis_css[] = "mdb.min.css";
-$mvc_vis_css[] = "bootstrap-extension.css";
-$mvc_vis_css[] = "jquery-ui.min.css";
-$mvc_vis_css[] = "main.css";
-$mvc_vis_css[] = "loader.css";
-$mvc_vis_css[] = "floating-label.css";
-$mvc_vis_css[] = "inputs.css";
-$mvc_vis_css[] = "jquery.sweet-modal.min.css";
-$mvc_vis_css[] = "gantt.css";
+Config::$mvc_vis_css[] = "bootstrap.min.css";
+Config::$mvc_vis_css[] = "mdb.min.css";
+Config::$mvc_vis_css[] = "bootstrap-extension.css";
+Config::$mvc_vis_css[] = "jquery-ui.min.css";
+Config::$mvc_vis_css[] = "main.css";
+Config::$mvc_vis_css[] = "loader.css";
+Config::$mvc_vis_css[] = "floating-label.css";
+Config::$mvc_vis_css[] = "inputs.css";
+Config::$mvc_vis_css[] = "jquery.sweet-modal.min.css";
+Config::$mvc_vis_css[] = "gantt.css";
 
 //JS
-$mvc_vis_scripts[] = "libs/jquery.min.js";
-$mvc_vis_scripts[] = "libs/jquery-ui.min.js";
-$mvc_vis_scripts[] = "libs/bootstrap.min.js";
-$mvc_vis_scripts[] = "libs/bootstrap.bundle.min.js";
-$mvc_vis_scripts[] = "libs/mdb.min.js";
-$mvc_vis_scripts[] = "libs/jquery.sweet-modal.min.js";
-$mvc_vis_scripts[] = "js/Modal.js";
-$mvc_vis_scripts[] = "js/utils.js";
-$mvc_vis_scripts[] = "js/ViewUtils.js";
-$mvc_vis_scripts[] = "js/inputs.js";
-$mvc_vis_scripts[] = "js/AjaxController.js";
-$mvc_vis_scripts[] = "js/Validator.js";
-$mvc_vis_scripts[] = "js/FormValidator.js";
-$mvc_vis_scripts[] = "js/preloader.js";
-$mvc_vis_scripts[] = "js/generalScript.js";
+Config::$mvc_vis_scripts[] = "libs/jquery.min.js";
+Config::$mvc_vis_scripts[] = "libs/jquery-ui.min.js";
+Config::$mvc_vis_scripts[] = "libs/bootstrap.min.js";
+Config::$mvc_vis_scripts[] = "libs/bootstrap.bundle.min.js";
+Config::$mvc_vis_scripts[] = "libs/mdb.min.js";
+Config::$mvc_vis_scripts[] = "libs/jquery.sweet-modal.min.js";
+Config::$mvc_vis_scripts[] = "js/Modal.js";
+Config::$mvc_vis_scripts[] = "js/utils.js";
+Config::$mvc_vis_scripts[] = "js/ViewUtils.js";
+Config::$mvc_vis_scripts[] = "js/inputs.js";
+Config::$mvc_vis_scripts[] = "js/AjaxController.js";
+Config::$mvc_vis_scripts[] = "js/Validator.js";
+Config::$mvc_vis_scripts[] = "js/FormValidator.js";
+Config::$mvc_vis_scripts[] = "js/preloader.js";
+Config::$mvc_vis_scripts[] = "js/generalScript.js";
