80f10f9efa88514c6920f62c90a094b7a4326a20
pepe_fabra_valverde <jofaval@outlook.com>
2020-04-18 15:23:05 +0200

    Change loader.css order

--- server/classes/config/resources.php
+++ server/classes/config/resources.php
@@ -1,12 +1,12 @@
 <?php
 
 //CSS
+Config::$mvc_vis_css[] = "loader.css";
 Config::$mvc_vis_css[] = "bootstrap.min.css";
 Config::$mvc_vis_css[] = "mdb.min.css";
 Config::$mvc_vis_css[] = "bootstrap-extension.css";
 Config::$mvc_vis_css[] = "jquery-ui.min.css";
 Config::$mvc_vis_css[] = "main.css";
-Config::$mvc_vis_css[] = "loader.css";
 Config::$mvc_vis_css[] = "floating-label.css";
 Config::$mvc_vis_css[] = "inputs.css";
 Config::$mvc_vis_css[] = "jquery.sweet-modal.min.css";
