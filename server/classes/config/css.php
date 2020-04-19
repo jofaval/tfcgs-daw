ad47707054a3472a4026f599fb8cb24fad279efb
pepe_fabra_valverde <jofaval@outlook.com>
2020-04-19 16:43:02 +0200

    Split config resources in correct files

---
+++ server/classes/config/css.php
@@ -0,0 +1,12 @@
+<?php
+
+Config::$mvc_vis_css[] = "loader.css";
+Config::$mvc_vis_css[] = "bootstrap.min.css";
+Config::$mvc_vis_css[] = "mdb.min.css";
+Config::$mvc_vis_css[] = "bootstrap-extension.css";
+Config::$mvc_vis_css[] = "jquery-ui.min.css";
+Config::$mvc_vis_css[] = "main.css";
+Config::$mvc_vis_css[] = "floating-label.css";
+Config::$mvc_vis_css[] = "inputs.css";
+Config::$mvc_vis_css[] = "jquery.sweet-modal.min.css";
+Config::$mvc_vis_css[] = "gantt.css";
--- server/classes/config/js.php
+++ server/classes/config/js.php
@@ -1,18 +1,5 @@
 <?php
 
-//CSS
-Config::$mvc_vis_css[] = "loader.css";
-Config::$mvc_vis_css[] = "bootstrap.min.css";
-Config::$mvc_vis_css[] = "mdb.min.css";
-Config::$mvc_vis_css[] = "bootstrap-extension.css";
-Config::$mvc_vis_css[] = "jquery-ui.min.css";
-Config::$mvc_vis_css[] = "main.css";
-Config::$mvc_vis_css[] = "floating-label.css";
-Config::$mvc_vis_css[] = "inputs.css";
-Config::$mvc_vis_css[] = "jquery.sweet-modal.min.css";
-Config::$mvc_vis_css[] = "gantt.css";
-
-//JS
 Config::$mvc_vis_scripts[] = "libs/jquery.min.js";
 Config::$mvc_vis_scripts[] = "libs/jquery-ui.min.js";
 Config::$mvc_vis_scripts[] = "libs/bootstrap.min.js";
