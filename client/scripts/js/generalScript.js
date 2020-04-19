d97b089f43600c30b81550e0fd5a68945eda4b5c
pepe_fabra_valverde <jofaval@outlook.com>
2020-04-19 16:38:23 +0200

    Add scroll on tracker with backToTop button

--- client/scripts/js/generalScript.js
+++ client/scripts/js/generalScript.js
@@ -2,4 +2,25 @@ $(window).on("load", function () {
     $(document.body).removeClass("text-light");
     $(document.body).addClass("text-dark");
     $(".hiddendiv.common").remove();
+
+    $("main").addClass("scrollOnTop");
+
+    $("main").on("scroll", function () {
+        var current = $(this);
+        var scroll = current.scrollTop();
+        var maxScrollHeight = current.innerHeight();
+        var height = current.get(0).scrollHeight;
+        //console.log(scroll, scroll + maxScrollHeight, height, (scroll + maxScrollHeight) == height);
+
+        if (scroll == 0) {
+            current.addClass("scrollOnTop");
+            current.removeClass("scrollOnBottom");
+        } else if (scroll + maxScrollHeight == height) {
+            current.addClass("scrollOnBottom");
+            current.removeClass("scrollOnTop");
+        } else {
+            current.removeClass("scrollOnTop");
+            current.removeClass("scrollOnBottom");
+        }
+    });
 });
