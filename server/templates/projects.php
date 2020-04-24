ba65ed61869b55ac3e4d405c2d4b637485a6b130
pepe_fabra_valverde <jofaval@outlook.com>
2020-04-24 23:03:38 +0200

    Updated projects base template

--- server/templates/projects.php
+++ server/templates/projects.php
@@ -21,9 +21,9 @@ $breadcrumb = [
 ?>
 
 <?php ob_start()?>
-<div class="container p-0">
+<div class=" p-0">
     <div class="row py-5 m-0 projectHeader">
-        <div class="projectImageContainer col-3">
+        <div class="projectImageContainercol-3">
             <img class="projectImage mx-5" src="../img/profile-pic.png" alt="" width="200">
         </div>
         <div class="display-none display-sm-block projectDetails text-white col">
@@ -62,111 +62,15 @@ $breadcrumb = [
         </div>
         <div class="btn btn-success btn-sm projectBtnAdd">+ Add</div>
         <div class="md-form input-group col my-2">
-            <input type="search" class="form-control pl-0 rounded-0" name="search" id="search" placeholder="Search...">
+            <input type="search" class="form-control text-white pl-0 rounded-0" name="projectSearch" id="projectSearch"
+                placeholder="Search...">
             <div class="input-group-append">
                 <span class="btn btn-sm btn-primary m-0 input-group-text md-addon">Search</span>
             </div>
         </div>
     </div>
-    <div class="grey darken-3 m-0 px-2 pt-2">
-        <div class="row projectCardRow d-flex justify-content-center m-0">
-            <div class="projectCard row col m-2 bg-white">
-                <div
-                    class="row projectCardDetails flex-wrap d-flex justify-content-start justify-items-center align-content-center align-items-center w-100 m-0 pt-2">
-                    <h5 class="projectCardTitle m-0 font-weight-bold">Project title</h5>
-                    <div class="btn btn-sm btn-primary">Go to project</div>
-                    <img src="" alt="" class="projectCardBookmarkedIcon">
-                    <div class="projectCardFlags float-right btn-group">
-                        <div class="projectsBtnBookmarked btn btn-sm btn-warning">Bookmarked</div>
-                        <div class="projectsBtnCreated btn btn-sm btn-success">Created</div>
-                    </div>
-                </div>
-                <div class="projectCardDescription text-justify my-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam repellendus voluptatem, dolorum
-                    nostrum sit iure qui amet maxime.
-                </div>
-            </div>
-            <div class="projectCard row col m-2 bg-white">
-                <div
-                    class="row projectCardDetails flex-wrap d-flex justify-content-start justify-items-center align-content-center align-items-center w-100 m-0 pt-2">
-                    <h5 class="projectCardTitle m-0 font-weight-bold">Project title</h5>
-                    <div class="btn btn-sm btn-primary">Go to project</div>
-                    <img src="" alt="" class="projectCardBookmarkedIcon">
-                    <div class="projectCardFlags float-right btn-group">
-                        <div class="projectsBtnShared btn btn-sm btn-primary">Shared</div>
-                    </div>
-                </div>
-                <div class="projectCardDescription text-justify my-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam repellendus voluptatem, dolorum
-                    nostrum sit iure qui amet maxime.
-                </div>
-            </div>
-        </div>
-        <div class="row projectCardRow d-flex justify-content-center m-0">
-            <div class="projectCard row col m-2 bg-white">
-                <div
-                    class="row projectCardDetails flex-wrap d-flex justify-content-start justify-items-center align-content-center align-items-center w-100 m-0 pt-2">
-                    <h5 class="projectCardTitle m-0 font-weight-bold">Project title</h5>
-                    <div class="btn btn-sm btn-primary">Go to project</div>
-                    <img src="" alt="" class="projectCardBookmarkedIcon">
-                    <div class="projectCardFlags float-right btn-group">
-                        <div class="projectsBtnCreated btn btn-sm btn-success">Created</div>
-                    </div>
-                </div>
-                <div class="projectCardDescription text-justify my-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam repellendus voluptatem, dolorum
-                    nostrum sit iure qui amet maxime.
-                </div>
-            </div>
-            <div class="projectCard row col m-2 bg-white">
-                <div
-                    class="row projectCardDetails flex-wrap d-flex justify-content-start justify-items-center align-content-center align-items-center w-100 m-0 pt-2">
-                    <h5 class="projectCardTitle m-0 font-weight-bold">Project title</h5>
-                    <div class="btn btn-sm btn-primary">Go to project</div>
-                    <img src="" alt="" class="projectCardBookmarkedIcon">
-                    <div class="projectCardFlags float-right btn-group">
-                        <div class="projectsBtnBookmarked btn btn-sm btn-warning">Bookmarked</div>
-                        <div class="projectsBtnShared btn btn-sm btn-primary">Shared</div>
-                    </div>
-                </div>
-                <div class="projectCardDescription text-justify my-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam repellendus voluptatem, dolorum
-                    nostrum sit iure qui amet maxime.
-                </div>
-            </div>
-        </div>
-        <div class="row projectCardRow d-flex justify-content-center m-0">
-            <div class="projectCard row col m-2 bg-white">
-                <div
-                    class="row projectCardDetails flex-wrap d-flex justify-content-start justify-items-center align-content-center align-items-center w-100 m-0 pt-2">
-                    <h5 class="projectCardTitle m-0 font-weight-bold">Project title</h5>
-                    <div class="btn btn-sm btn-primary">Go to project</div>
-                    <img src="" alt="" class="projectCardBookmarkedIcon">
-                    <div class="projectCardFlags float-right btn-group">
-                        <div class="projectsBtnShared btn btn-sm btn-primary">Shared</div>
-                    </div>
-                </div>
-                <div class="projectCardDescription text-justify my-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam repellendus voluptatem, dolorum
-                    nostrum sit iure qui amet maxime.
-                </div>
-            </div>
-            <div class="projectCard row col m-2 bg-white">
-                <div
-                    class="row projectCardDetails flex-wrap d-flex justify-content-start justify-items-center align-content-center align-items-center w-100 m-0 pt-2">
-                    <h5 class="projectCardTitle m-0 font-weight-bold">Project title</h5>
-                    <div class="btn btn-sm btn-primary">Go to project</div>
-                    <img src="" alt="" class="projectCardBookmarkedIcon">
-                    <div class="projectCardFlags float-right btn-group">
-                        <div class="projectsBtnCreated btn btn-sm btn-success">Created</div>
-                    </div>
-                </div>
-                <div class="projectCardDescription text-justify my-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam repellendus voluptatem, dolorum
-                    nostrum sit iure qui amet maxime.
-                </div>
-            </div>
-        </div>
+    <div class="grey darken-3 m-0 mx-auto px-2 pt-2">
+        <div class="projectsContainer w-100 m-0"></div>
         <div class="row d-flex justify-content-center py-3">
             <nav aria-label="Page navigation example" class="bg-transparent">
                 <ul class="pagination pg-blue mb-0">
