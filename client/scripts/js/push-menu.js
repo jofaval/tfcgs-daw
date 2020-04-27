e403136e31a00a33dfbf776eba00c8a74e26fea1
pepe_fabra_valverde <jofaval@outlook.com>
2020-04-27 20:39:02 +0200

    Updated pushmenu overlapping bug

--- client/scripts/js/push-menu.js
+++ client/scripts/js/push-menu.js
@@ -10,7 +10,7 @@ pushMenuContainer.prepend($(".pushMenuOverlap"));
 
 $("main").toggleClass("flex-column flex-row");
 
-$("#pushMenuToggleBtn, .pushMenu").on("click", function (event) {
+$("#pushMenuToggleBtn, .pushMenu, .pushMenuOverlap").on("click", function (event) {
     var event = event || window.event;
     event.preventDefault();
     event.stopPropagation();
@@ -20,19 +20,13 @@ $("#pushMenuToggleBtn, .pushMenu").on("click", function (event) {
     if (toggled) {
         hidePushMenu();
     }
+
+    toggled = !toggled;
     $("#pushMenuToggleBtn").html(toggled ? "->" : "<-");
 
     console.log(toggled);
 
-    toggled = !toggled;
 });
-
-$(".pushMenu a").on("click", function (event) {
-    var event = event || window.event;
-    event.stopPropagation();
-});
-
-$("#pushMenuToggleBtn").trigger("click");
 
 pushMenuContainer.on("click", function (event) {
     var event = event || window.event;
@@ -46,26 +40,49 @@ pushMenuContainer.on("click", function (event) {
     console.log(toggled);
 
     toggled = false;
+});
 
+$(".pushMenu a").on("click", function (event) {
+    var event = event || window.event;
+    event.stopPropagation();
+});
+
+$(".pushMenu").trigger("click");
+
+pushMenuContainer.on("click", function (event) {
+    var event = event || window.event;
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (toggled) {
+        $("#pushMenuToggleBtn").trigger("click");
+    }
+
+    console.log(toggled);
+
+    toggled = false;
 });
 
 function hidePushMenu() {
     $(".pushMenuOverlap").stop().animate({
         backgroundColor: "rgba(0, 0, 0, 0)",
-    }).css("pointer-events", "all");
+    }).addClass("click-through");
 
     pushMenu.stop().animate({
-        marginLeft: `-${pushMenu.width()}px`,
+        marginLeft: `-${pushMenu.width()/16}em`,
     }, 200);
     $("#pushMenuToggleBtn").html(!toggled ? "->" : "<-");
+
+    //toggled = false;
 }
 
 function showPushMenu() {
     $(".pushMenuOverlap").stop().animate({
         backgroundColor: "rgba(0, 0, 0, 0.2)",
-    }).css("pointer-events", "none");
-
+    }).removeClass("click-through");
     pushMenu.stop().animate({
         marginLeft: "0px",
     }, 200);
+
+    //toggled = true;
 }
