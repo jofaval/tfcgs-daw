6115dc6455fc3063b9dcc180f6135d673ce1e4ea
pepe_fabra_valverde <jofaval@outlook.com>
2020-04-27 21:30:26 +0200

    Added save button in diary

--- server/templates/project/diary.php
+++ server/templates/project/diary.php
@@ -33,15 +33,16 @@
 
 <div class="w-100 h-100 tabContent d-block" id="tabContent3">
     <div class="mx-auto">
-        <button class="btn m-3 btn-primary" id="navigationSchemeBtn">Generate navigation scheme</button>
         <div class="form-row d-flex py-4">
+            <button class="btn btn-sm m-3 btn-primary" id="navigationSchemeBtn">Generate navigation scheme</button>
+            <button class="btn btn-sm m-3 btn-primary diaryBtnSave" id="diaryBtnSave">Save</button>
             <div class="col-sm-3 rounded order-0 order-sm-2 bg-white shadow mx-1">
                 <div class="md-form m-0">
                     <input placeholder="Select a date" type="text" id="datepicker" class="form-control datepicker">
                 </div>
             </div>
-            <button class="btn btn-sm btn-primary order-2 order-sm-1 text-white ml-auto">&lt;</button>
-            <button class="btn btn-sm btn-primary order-3 text-white mr-auto">&gt;</button>
+            <button class="btn btn-sm btn-primary order-2 order-sm-1 text-white">&lt;</button>
+            <button class="btn btn-sm btn-primary order-3 text-white">&gt;</button>
             <br>
         </div>
         <div class="mx-sm-5 mb-sm-5 summernoteContainer">
