98a06e0ad76fa3ebab36cd409380edbe358fa274
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-03 14:41:36 +0200

    Add extra responsiveness tag

--- server/templates/layout.php
+++ server/templates/layout.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="/daw/img/branding/favicon-16x16.png" type="image/x-icon">
     <link rel="shortcut icon" href="/daw/img/branding/favicon-32x32.png" type="image/x-icon">
     <link rel="shortcut icon" href="/daw/img/branding/favicon-96x96.png" type="image/x-icon">
-
+    <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <style>
@@ -55,9 +55,8 @@
             style="transition: all 0.2s ease-in-out 0s;">
             <ul class="navbar-nav" style="transition: all 0.2s ease-in-out 0s;">
                 <li class="nav-item active" style="transition: all 0.2s ease-in-out 0s;">
-                    <a class="nav-link" href="/daw/projects/"
-                        style="transition: all 0.2s ease-in-out 0s;">Schedules<span class="sr-only"
-                            style="transition: all 0.2s ease-in-out 0s;">(current)</span></a>
+                    <a class="nav-link" href="/daw/projects/" style="transition: all 0.2s ease-in-out 0s;">Origen<span
+                            class="sr-only" style="transition: all 0.2s ease-in-out 0s;">(current)</span></a>
                 </li>
                 <li class="nav-item" style="transition: all 0.2s ease-in-out 0s;">
                     <a class="nav-link selected" href="/daw/projects/"
--- server/templates/layoutProject.php
+++ server/templates/layoutProject.php
@@ -8,6 +8,7 @@
     <link rel="shortcut icon" href="/daw/img/branding/favicon-16x16.png" type="image/x-icon">
     <link rel="shortcut icon" href="/daw/img/branding/favicon-32x32.png" type="image/x-icon">
     <link rel="shortcut icon" href="/daw/img/branding/favicon-96x96.png" type="image/x-icon">
+    <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <style>
