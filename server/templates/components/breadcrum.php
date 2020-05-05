06ed8e07907de44f0df5871fc2ded8c4978cd89f
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-05 15:42:39 +0200

    Improved breadcrum UX

--- server/templates/components/breadcrum.php
+++ server/templates/components/breadcrum.php
@@ -1,11 +1,12 @@
 <section class="w-100 breadcrumbContainer" aria-label="breadcrumb">
     <ol class="breadcrumb mb-0 rounded-0">
         <?php foreach ($breadcrumb as $elem): ?>
-        <li class="breadcrumb-item"><a class="<?php echo $elem["active"] ? "text-muted" : ""; ?>"
-                href="<?php echo $elem["link"]; ?>">
+        <li class="breadcrumb-item">
+            <a class="<?php echo $elem["active"] ? "" : "text-muted"; ?>" href="<?php echo $elem["link"]; ?>">
                 <span><i class="fa fa-<?php echo $elem["icon"]; ?>"></i></span>
-                <span class=""><?php echo $elem["name"]; ?></span>
-            </a></li>
+                <span class="text-overflow-ellipsis max-text-10"><?php echo $elem["name"]; ?></span>
+            </a>
+        </li>
         <?php endforeach;?>
     </ol>
 </section>
