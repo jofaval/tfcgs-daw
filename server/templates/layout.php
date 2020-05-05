7f7c6ca68510320aa565fef9b83c970b8fcb6d2c
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-05 14:05:27 +0200

    Added icon support for breadcrum

--- server/templates/layout.php
+++ server/templates/layout.php
@@ -78,7 +78,10 @@
         <ol class="breadcrumb mb-0 rounded-0">
             <?php foreach ($breadcrumb as $elem): ?>
             <li class="breadcrumb-item"><a class="<?php echo $elem["active"] ? "text-muted" : ""; ?>"
-                    href="<?php echo $elem["link"]; ?>"><?php echo $elem["name"]; ?></a></li>
+                    href="<?php echo $elem["link"]; ?>">
+                    <span><i class="fa fa-<?php echo $elem["icon"]; ?>"></i></span>
+                    <span class=""><?php echo $elem["name"]; ?></span>
+                </a></li>
             <?php endforeach;?>
         </ol>
     </section>
