73054137982bd59da1dd40d3b2093c8a686fd28b
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-05 21:43:13 +0200

    Moved addNewRoute to OnlyAdmin

--- server/OnlyAdmin.php
+++ server/OnlyAdmin.php
@@ -1,3 +1,4 @@
 <?php
 
 $map['accessLevel'] = array('controller' => 'Controller', 'action' => 'accessLevel', 'access' => Config::$ACCESS_LEVEL_GUEST);
+$map['addNewRoute'] = array('controller' => 'Controller', 'action' => 'addNewRoute', 'access' => Config::$ACCESS_LEVEL_GUEST);
