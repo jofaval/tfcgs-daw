5aa323570ed6ee453a6e51e2b692f18eb873b7de
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-06 19:10:45 +0200

    Added change-pass view

---
+++ server/templates/profile/pass.php
@@ -0,0 +1,29 @@
+<div class="card-body card-body-cascade text-center mt-3">
+    <form class="" action="/daw/profile/" method="POST">
+        <div class="row">
+            <div class="col-md-6 mx-auto">
+                <div class="md-form mb-0">
+                    <input type="password" name="password" id="password" class="form-control validate">
+                    <label for="password" data-error="wrong" data-success="right">Contraseña</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 mx-auto">
+                <div class="md-form mb-0">
+                    <input type="password" name="repeatPassword" id="repeatPassword" class="form-control validate">
+                    <label for="repeatPassword" data-error="wrong" data-success="right">Repetir Contraseña</label>
+                </div>
+            </div>
+        </div>
+        <?php if ($viewParams["editable"]): ?>
+        <div class="row">
+            <div class="col-md-12 text-center my-4">
+                <span class="waves-input-wrapper waves-effect waves-light"><input type="submit"
+                        value="Cambiar contraseña" name="updatePassword" id="updatePassword"
+                        class="btn btn-primary btn-rounded"></span>
+            </div>
+        </div>
+        <?php endif;?>
+    </form>
+</div>
