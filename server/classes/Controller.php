dd5bdcc6f3ea1bd06351230b7c85309b3b8e33bd
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-06 22:30:28 +0200

    Added resize on upload

--- server/classes/Controller.php
+++ server/classes/Controller.php
@@ -86,6 +86,8 @@ class Controller
                 <p class='m-0'>No se ha subido la imagen.</p>\n
                 </div>";
                 } else {
+                    resizeImage($profileImage,
+                        400, 400);
                     header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
                     header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
                     header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
@@ -103,6 +105,8 @@ class Controller
                 <p class='m-0'>No se ha subido la imagen.</p>\n
                 </div>";
                 } else {
+                    resizeImage($bgImage,
+                        1750, 400);
                     header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
                     header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
                     header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
