5ab0841b906d461f0db75b4715d15b1d977350db
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-06 12:03:59 +0200

    Added UTF-8 charset

--- server/templates/components/head.php
+++ server/templates/components/head.php
@@ -5,6 +5,7 @@
     <link rel="shortcut icon" href="/daw/img/branding/favicon-96x96.png" type="image/x-icon">
     <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <style>
     /*Avoid flashing the user with white background*/
