1f0f34bf7c8ecbce5f57029de0b2edca16b63882
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-07 10:32:28 +0200

    Changed details view

--- server/templates/project/details.php
+++ server/templates/project/details.php
@@ -1,14 +1,69 @@
 <?php $projectData = $viewParams["projectData"];?>
-<div class="w-100 h-100 tabContent d-block" id="tabContent5">
+<div class="w-100 h-100 tabContent container p-0 d-block" id="tabContent5">
     <div class="content px-5">
-        <div class="text-white mt-5 h5">Project title</div>
-        <div class="text-white font-weight-bold mb-5 h3"><?php echo $projectData["projectTitle"]; ?></div>
-        <div class="text-white h5">Project description</div>
-        <div class="md-form mb-5 mt-0">
-            <textarea id="form7" class="md-textarea form-control text-white p-0" disabled
-                rows="3"><?php echo $projectData["projectDescription"]; ?></textarea>
-            <label for="form7" class="d-none">Material textarea</label>
-        </div>
+        <form action="/daw/projects/id/<?php echo $viewParams["id"] ?>/details/">
+            <div class="text-white mt-5 h5">Project title</div>
+            <div class="md-form mb-5">
+                <input class="form-control text-white font-weight-bold h3" disabled="" name="projectTitle"
+                    id="projectTitle" value="<?php echo $projectData["projectTitle"]; ?>" />
+                <label for="projectTitle"></label>
+            </div>
+            <div class="text-white h5">Project description</div>
+            <div class="md-form mb-5 mt-0">
+                <textarea id="form7" class="md-textarea form-control text-white p-0" disabled
+                    rows="3"><?php echo $projectData["projectDescription"]; ?></textarea>
+                <label for="form7" class="d-none">Material textarea</label>
+            </div>
+            <?php if (Sessions::getInstance()->getSession("access") >= Config::$ACCESS_LEVEL_ADMIN): ?>
+            <div class="row mx-auto flex-center">
+                <button class="btn btn-primary" name="changeProjectDetails" type="submit">Cambiar detalles</button>
+            </div>
+            <?php endif;?>
+        </form>
+        <?php if (Sessions::getInstance()->getSession("access") >= Config::$ACCESS_LEVEL_ADMIN): ?>
+        <form class="" action="/daw/projects/id/<?php echo $viewParams["id"] ?>/details/" method="POST"
+            enctype="multipart/form-data">
+            <h1 class="text-left text-white">Imagen de fondo</h1>
+            <div class="row">
+                <div class="col-md-6 mx-auto">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="profileImageCaption">Elegir una imagen</span>
+                        </div>
+                        <div class="custom-file text-left">
+                            <input type="file" required="" class="custom-file-input" name="profileImage"
+                                id="profileImage" aria-describedby="profileImageCaption" accept=".gif,.jpg,.jpeg,.png">
+                            <label class="custom-file-label" for="profileImage">image.png</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mx-auto flex-center">
+                <button class="btn btn-primary" name="changeProjectProfileImage" type="submit">Cambiar imagen</button>
+            </div>
+        </form>
+        <form class="" action="/daw/projects/id/<?php echo $viewParams["id"] ?>/details/" method="POST"
+            enctype="multipart/form-data">
+            <h1 class="text-left text-white">Imagen de fondo</h1>
+            <div class="row">
+                <div class="col-md-6 mx-auto">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="bgImageCaption">Elegir una imagen</span>
+                        </div>
+                        <div class="custom-file text-left">
+                            <input type="file" required="" class="custom-file-input" name="bgImage" id="bgImage"
+                                aria-describedby="bgImageCaption" accept=".gif,.jpg,.jpeg,.png">
+                            <label class="custom-file-label" for="bgImage">image.png</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mx-auto flex-center">
+                <button class="btn btn-primary" name="changeProjectBGImage" type="submit">Cambiar imagen</button>
+            </div>
+        </form>
+        <?php endif;?>
         <div class="projectCreationDetails mb-5 text-muted">
             <?php if (!is_null($projectData["collaborator"])): ?>
             <div class="float-right text-right">
@@ -27,6 +82,9 @@
                     class="projectCreatedBy"><?php echo $projectData["projectCreator"]; ?></a>
                 <br> el <span class="projectCreationDate"><?php echo $projectData["projectCreationDate"]; ?></span>
             </div>
+        </div>
+        <div class="row mx-auto flex-center">
+
         </div>
     </div>
 </div>
