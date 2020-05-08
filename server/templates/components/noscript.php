0b6d621ad26008afc6b13d0f654af74fa315ec24
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-08 08:13:32 +0200

    Added noscript component

---
+++ server/templates/components/noscript.php
@@ -0,0 +1,6 @@
+<noscript class="position-absolute w-100 h-100 d-flex justify-content-center align-items-center" id="noScript">
+    <div class="main m-5 p-5 w-75 h-75 rounded shadow bg-white d-flex justify-content-center align-items-center">
+        <h1>Para el correcto funcionamiento de esta aplicación se <span class="font-weight-bold">JavaScript</span>.
+            <br>Sentimos las molestias</h1>
+    </div>
+</noscript>
--- server/templates/layoutProject.php
+++ server/templates/layoutProject.php
@@ -7,12 +7,7 @@
 
 <body class="text-light overflow-hidden">
     <?php require_once __DIR__ . "/components/loader.php";?>
-    <noscript class="position-absolute w-100 h-100 d-flex justify-content-center align-items-center" id="noScript">
-        <div class="main m-5 p-5 w-75 h-75 rounded shadow bg-white d-flex justify-content-center align-items-center">
-            <h1>Para el correcto funcionamiento de esta aplicación se <span class="font-weight-bold">JavaScript</span>.
-                <br>Sentimos las molestias</h1>
-        </div>
-    </noscript>
+    <?php require_once __DIR__ . "/components/noscript.php";?>
     <?php if ($showHeader):
     require_once __DIR__ . "/components/header.php";
 endif;?>
