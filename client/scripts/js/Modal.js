4ba3c7eef2f703e78433ba6731e2a66af64ef8f0
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-08 12:23:04 +0200

    Improved ModalUtils.js

--- client/scripts/js/Modal.js
+++ client/scripts/js/Modal.js
@@ -1,33 +1,47 @@
-class Modal {
+LIGHT_THEME = [
+    "dark-overlay",
+    "light-modal"
+];
+MIXED_THEME = [
+    "dark-overlay",
+    "light-modal"
+];
+DARK_THEME = [
+    "dark-overlay",
+    "dark-modal",
+];
+THEME = MIXED_THEME;
 
-    LIGHT_THEME = $.sweetModal.THEME_MIXED;
-    DARK_THEME = $.sweetModal.THEME_DARK;
-    THEME = LIGHT_THEME;
+class Modal {
 
     /* 
     settings = {
-        title: "test",
-        content: "test",
-        onOpen: function () {},
-        onAccept: function () {},
-        onCancel: function () {},
-        onClose: function () {},
+        "title": "test",
+        "content": "test",
+        "onOpen": function () {},
+        "onAccept": function () {},
+        "onCancel": function () {},
+        "onClose": function () {},
     }
     */
     static modal(settings) {
+        console.log(settings);
+
         var modal = $.sweetModal({
-            title: settings.title,
-            content: settings.content,
-            theme: $.sweetModal.THEME_DARK
+            "title": settings["title"],
+            "content": settings["content"],
+            "theme": THEME
         });
 
         modal.params["onOpen"] = function () {
-            settings.onOpen(modal);
+            settings["onOpen"](modal);
         };
 
         modal.params["onClose"] = function () {
-            settings.onOpen(modal);
+            settings["onOpen"](modal);
         };
+
+        return modal;
     }
 
     /* 
@@ -41,19 +55,35 @@ class Modal {
     }
     */
     static confirmationModal(settings) {
-        var modal = $.sweetModal.confirm(settings.title, settings.title, function () {
-            settings.onAccept(modal);
-        }, function () {
-            settings.onCancel(modal);
+        var confirm = $.confirm({
+            "title": settings["title"],
+            "message": settings["body"],
+            "theme": THEME,
+            "buttons": {
+                "Cancelar": {
+                    "class": "greenB",
+                    "action": function () {
+                        settings["onCancel"](confirm);
+                    }
+                },
+                "Confirmar": {
+                    "class": "redB",
+                    "action": function () {
+                        settings["onAccept"](confirm);
+                    }
+                },
+            }
         });
 
-        modal.params["onOpen"] = function () {
-            settings.onOpen(modal);
+        confirm.params["onOpen"] = function () {
+            settings["onOpen"](confirm);
         };
 
-        modal.params["onClose"] = function () {
-            settings.onOpen(modal);
+        confirm.params["onClose"] = function () {
+            settings["onOpen"](confirm);
         };
+
+        return confirm;
     }
 
     /* 
@@ -64,19 +94,22 @@ class Modal {
         onAccept: function () {},
         onCancel: function () {},
         onClose: function () {},
+        theme: theme,
     }
     */
     static specialAlert(settings) {
         var alert = $.sweetModal({
-            content: settings.title,
-            icon: settings.error ? $.sweetModal.ICON_ERROR : $.sweetModal.ICON_SUCCESS,
-            theme: $.sweetModal.THEME_DARK,
+            "content": settings["title"],
+            "icon": settings["error"] ? $.sweetModal.ICON_ERROR : $.sweetModal.ICON_SUCCESS,
+            "theme": THEME,
         });
         successAlert.params["onOpen"] = function () {
-            settings.onOpen(alert);
+            settings["onOpen"](alert);
         }
         successAlert.params["onClose"] = function () {
-            settings.onClose(alert);
+            settings["onClose"](alert);
         }
+
+        return modal;
     }
 }
