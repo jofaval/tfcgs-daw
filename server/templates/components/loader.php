4716d2c40ca6b83546fff7f00ff991850cebaa7d
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-08 08:12:49 +0200

    Added load as first element to load

--- server/templates/components/loader.php
+++ server/templates/components/loader.php
@@ -1,4 +1,4 @@
-<div id="loader-wrapper z-index-overlap-top">
+<div id="loader-wrapper" class="z-index-overlap-top">
     <div id="loader"></div>
     <div class="loader-section section-left"></div>
     <div class="loader-section section-right"></div>
--- server/templates/layoutProject.php
+++ server/templates/layoutProject.php
@@ -6,6 +6,7 @@
 <?php require_once __DIR__ . "/components/head.php"?>
 
 <body class="text-light overflow-hidden">
+    <?php require_once __DIR__ . "/components/loader.php";?>
     <noscript class="position-absolute w-100 h-100 d-flex justify-content-center align-items-center" id="noScript">
         <div class="main m-5 p-5 w-75 h-75 rounded shadow bg-white d-flex justify-content-center align-items-center">
             <h1>Para el correcto funcionamiento de esta aplicación se <span class="font-weight-bold">JavaScript</span>.
