166c59875fde915519fb3c31118e7181d146b055
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-09 20:16:40 +0200

    Changed search bar position

--- server/templates/profile.php
+++ server/templates/profile.php
@@ -43,49 +43,9 @@ if (!$viewParams["editable"]) {
 <div class="container-fluid my-3">
     <section class="section">
         <div class="row">
-            <div class="col-lg-4 mb-4">
-                <div class="card card-cascade narrower shadow-none">
-                    <?php if ($viewParams["editable"]): ?>
-                    <div class="view view-cascade bg-primary lighten-3 shadow mx-3">
-                        <h5 class="mb-0 font-weight-bold text-center p-3 text-white">Editar Imagen</h5>
-                    </div>
-                    <?php endif;?>
-                    <div class="card-body card-body-cascade text-center">
-                        <img src="/daw/img/users/<?php echo $viewParams["profile"]["username"]; ?>/<?php echo $viewParams["profile"]["username"]; ?>.png"
-                            width="200" alt="User Photo" class="z-depth-1 mb-3 mx-auto">
-                        <?php if ($viewParams["editable"]): ?>
-                        <div class="row flex-center">
-                            <a href="/daw/profile/change-image/"
-                                class="btn btn-primary btn-rounded btn-sm waves-effect waves-light"><span><i
-                                        class="fa fa-upload"></i></span> Subir
-                                foto</a><br>
-                            <a href="/daw/profile/reset-profile-image/"
-                                class="btn btn-danger btn-rounded btn-sm waves-effect waves-light"><span><i
-                                        class="fa fa-trash"></i></span> Eliminar imagen de perfil</a>
-                            <a href="/daw/profile/reset-background-image/"
-                                class="btn btn-danger btn-rounded btn-sm waves-effect waves-light"><span><i
-                                        class="fa fa-trash"></i></span> Eliminar imagen de fondo</a>
-                        </div>
-                        <?php endif;?>
-                    </div>
-                </div>
-                <?php if ($viewParams["editable"]): ?>
-                <div class="card card-cascade my-3 narrower shadow-none">
-                    <div class="row flex-center my-3">
-                        <!-- <button class="btn btn-danger btn-rounded waves-effect waves-light">Deshabilitar
-                            cuenta</button> -->
-                        <a href="/daw/signout/" class="btn btn-danger btn-rounded waves-effect waves-light">
-                            <span><i class="fa fa-sign-out"></i></span> Cerrar sesión</a>
-                        <a href="/daw/profile/<?php echo $username ?>/"
-                            class="btn btn-primary btn-rounded waves-effect waves-light"><span><i
-                                    class="fa fa-eye"></i></span> Ver
-                            público</a>
-                    </div>
-                </div>
-                <?php endif;?>
-            </div>
-            <div class="col-lg-8 mb-4">
-                <div class="searchUserProfileContainer col-sm-4 my-sm-3 ml-auto px-3 py-1 bg-white rounded shadow">
+            <div class="col-lg-4 mb-4 pr-sm-0">
+                <!-- col-sm-4 -->
+                <div class="searchUserProfileContainer mb-sm-3 ml-auto px-3 py-1 bg-white rounded-0 shadow">
                     <!-- <form action="/daw/profile/" id="searchUserProfileForm" class="p-3 bg-white rounded shadow">
                         <div class="input-group md-form form-sm form-2 pl-0">
                             <input class="form-control my-0 py-1" type="text" id="username" name="username"
@@ -97,6 +57,50 @@ if (!$viewParams["editable"]) {
                         </div>
                     </form> -->
                 </div>
+                <div class="card card-cascade narrower shadow-none rounded-0">
+                    <?php if ($viewParams["editable"]): ?>
+                    <div class="view view-cascade bg-primary lighten-3 shadow mx-3">
+                        <h5 class="mb-0 font-weight-bold text-center p-3 text-white">Editar Imagen</h5>
+                    </div>
+                    <?php endif;?>
+                    <div class="card-body card-body-cascade text-center rounded-0">
+                        <img src="/daw/img/users/<?php echo $viewParams["profile"]["username"]; ?>/<?php echo $viewParams["profile"]["username"]; ?>.png"
+                            width="200" alt="User Photo" class="z-depth-1 mb-3 mx-auto">
+                        <p class="imUsername mb-0 font-weight-bold">
+                            <?php echo $viewParams["profile"]["username"]; ?>
+                        </p>
+                        <?php if ($viewParams["editable"]): ?>
+                        <div class="row flex-center">
+                            <a href="/daw/profile/change-image/"
+                                class="btn btn-primary btn-sm btn-sm waves-effect waves-light"><span><i
+                                        class="fa fa-upload"></i></span> Subir
+                                foto</a><br>
+                            <a href="/daw/profile/reset-profile-image/"
+                                class="btn btn-danger btn-sm btn-sm waves-effect waves-light"><span><i
+                                        class="fa fa-trash"></i></span> Eliminar imagen de perfil</a>
+                            <a href="/daw/profile/reset-background-image/"
+                                class="btn btn-danger btn-sm btn-sm waves-effect waves-light"><span><i
+                                        class="fa fa-trash"></i></span> Eliminar imagen de fondo</a>
+                        </div>
+                        <?php endif;?>
+                    </div>
+                </div>
+                <?php if ($viewParams["editable"]): ?>
+                <div class="card card-cascade my-3 narrower shadow-none">
+                    <div class="row flex-center my-3">
+                        <!-- <button class="btn btn-danger btn-sm waves-effect waves-light">Deshabilitar
+                            cuenta</button> -->
+                        <a href="/daw/signout/" class="btn btn-danger btn-sm waves-effect waves-light">
+                            <span><i class="fa fa-sign-out"></i></span> Cerrar sesión</a>
+                        <a href="/daw/profile/<?php echo $username ?>/"
+                            class="btn btn-primary btn-sm waves-effect waves-light"><span><i
+                                    class="fa fa-eye"></i></span> Ver
+                            público</a>
+                    </div>
+                </div>
+                <?php endif;?>
+            </div>
+            <div class="col-lg-8 mb-4">
                 <?php if (!$viewParams["editable"]): ?>
                 <img class="profileBackgroundImage w-100 d-none d-sm-block" height="300"
                     src="/daw/img/users/<?php echo $username; ?>/bg-<?php echo $username; ?>.png" alt="">
