40238d09a20af9407b8c5ebc89b67c81399dc6bb
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-11 20:11:01 +0200

    Added to font-weight-normal originalDate

--- client/scripts/webcomponents/TimeFromMoment.js
+++ client/scripts/webcomponents/TimeFromMoment.js
@@ -5,7 +5,7 @@ class TimeFromMoment {
         instance.htmlContent = $(`
         <div class="timeOutContainer d-inline-flex mx-1 row">
             <p class="timeOutDate mb-0"></p>
-            <p class="timeOutOriginal mb-0 ml-2 text-muted"></p>
+            <p class="timeOutOriginal font-weight-normal mb-0 ml-2 text-muted"></p>
         </div>
         `);
 
