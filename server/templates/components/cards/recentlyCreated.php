e41e086b2de4e1f7595068ce9cd7a4abc9a1cd73
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-12 14:38:07 +0200

    Added recentlyCard redirection

--- server/templates/components/cards/recentlyCreated.php
+++ server/templates/components/cards/recentlyCreated.php
@@ -5,7 +5,7 @@
         <h5 class="recentlyCardTitle max-text-10 text-dark text-overflow-ellipsis overflow-hidden m-0 font-weight-bold">
             <?php echo $recentlyCreated["title"]; ?></h5>
         <div class="recentlyCardFlags ml-auto btn-group">
-            <a href="/daw/projects/id/<?php echo $viewParams["id"]; ?>/dashboards/<?php echo $recentlyCreated["dashboard_title"]; ?>/"
+            <a href="/daw/projects/id/<?php echo $viewParams["id"]; ?>/dashboards/<?php echo $recentlyCreated["dashboard_title"]; ?>/task/id/<?php echo $recentlyCreated["id"]; ?>/"
                 class="btn btn-sm btn-primary mt-auto mr-2 mb-2 recentlyCardBtnView">Ir al tablero</a>
         </div>
     </div>
