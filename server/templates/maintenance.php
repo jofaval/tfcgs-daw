5a7c2b57a7cfdcb235fb873e8e7115795dafd5d8
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-12 21:11:39 +0200

    Updated maintenante visual style

--- server/templates/maintenance.php
+++ server/templates/maintenance.php
@@ -1,5 +1,5 @@
 <!--Page configuration-->
-<?php $optionalCSS = [];?>
+<?php $optionalCSS = ["animation.css"];?>
 <?php $optionalScripts = [];?>
 <?php $title = "Mantenimiento";?>
 <?php $showFooter = false;?>
@@ -24,7 +24,36 @@ $breadcrumb = [
 
 <?php ob_start()?>
 
-<h3> Estamos en mantenimiento! </h3>
+<style>
+.fa-big {
+    font-size: 10rem !important;
+}
+
+.fa-bigger {
+    font-size: 12.5rem !important;
+}
+
+main {
+    background: none !important;
+}
+</style>
+
+<div class="w-100 h-100 d-flex text-dark justify-content-center align-content-center
+ flex-column justify-items-center align-items-center text-center">
+    <h1>La aplicación se encuentra en mantenimiento</h1>
+    <div class="cogsContainer">
+        <span>
+            <i class="fa fa-cog fa-big rotate-self-animation"></i>
+        </span>
+        <span style="margin-left: -1em;">
+            <i class="fa fa-cog fa-bigger animation-delay-4 rotate-self-animation animation-reverse"></i>
+        </span>
+    </div>
+    <p class="font-weight-bold mb-0 h1">Perdonen las molestias</p>
+</div>
+<script>
+document.querySelector("main").classList.add("bg-warning");
+</script>
 
 <?php $contenido = ob_get_clean()?>
 
