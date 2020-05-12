d331c050a4e6f3c998cbf252187259c4cb44047d
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-12 16:15:47 +0200

    Added admin routes access

--- server/OnlyAdmin.php
+++ server/OnlyAdmin.php
@@ -1,4 +1,4 @@
 <?php
 
-$map['accessLevel'] = array('controller' => 'Controller', 'action' => 'accessLevel', 'access' => Config::$ACCESS_LEVEL_GUEST);
-$map['addNewRoute'] = array('controller' => 'Controller', 'action' => 'addNewRoute', 'access' => Config::$ACCESS_LEVEL_GUEST);
+$map['accessLevel'] = array('controller' => 'Controller', 'action' => 'accessLevel', 'access' => Config::$ACCESS_LEVEL_ADMIN);
+$map['addNewRoute'] = array('controller' => 'Controller', 'action' => 'addNewRoute', 'access' => Config::$ACCESS_LEVEL_ADMIN);
