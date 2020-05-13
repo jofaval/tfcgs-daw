d94e6c0d243894889ece8c15077da821835429fb
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-13 15:51:52 +0200

    Removed shadow and title on phone version actionCard

--- server/templates/components/cards/actionCard.php
+++ server/templates/components/cards/actionCard.php
@@ -10,13 +10,13 @@
             <?php echo $action["name"]; ?></p>
     </div>
 </a>
-<a href="<?php echo $action["link"]; ?>" title="<?php /* echo $action["name"]; */?>"
+<a href="<?php echo $action["link"]; ?>"
     class="action d-sm-none w-100 cursor-pointer px-0 shadow-none hoverable <?php echo $action["color"]; ?> m-2"
     id="action<?php echo $action["id"]; ?>">
     <div class="actionIcon img-fluid h-75 waves-light center-elements text-white p-3">
         <i class="fa fa-<?php echo $action["icon"]; ?>"></i>
     </div>
-    <div class="actionTitle h-25 w-100 .z-depth-1 text-center d-flex justify-content-center py-2 text-white bg-dark">
+    <div class="actionTitle h-25 w-100 text-center d-flex justify-content-center py-2 text-white bg-dark">
         <p class="align-self-center white-text fixed-line-spacing mb-0">
             <?php echo $action["name"]; ?></p>
     </div>
