fb2b6622beae20254435a4ba695f6d05e8aa264c
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-13 16:51:36 +0200

    Fixed phone icon displaying in desktop

--- server/templates/components/breadcrum.php
+++ server/templates/components/breadcrum.php
@@ -3,7 +3,7 @@
         <?php foreach ($breadcrumb as $elem): ?>
         <li class="breadcrumb-item">
             <a class="<?php echo $elem["active"] ? "" : "text-muted"; ?>" href="<?php echo $elem["link"]; ?>">
-                <span class="d--smnone"><i class="fa fa-lg fa-<?php echo $elem["icon"]; ?>"></i></span>
+                <span class="d-sm-none"><i class="fa fa-lg fa-<?php echo $elem["icon"]; ?>"></i></span>
                 <span class="d-none d-sm-inline"><i class="fa fa-<?php echo $elem["icon"]; ?>"></i></span>
                 <span class="text-overflow-ellipsis d-none d-sm-inline max-text-10"><?php echo $elem["name"]; ?></span>
             </a>
