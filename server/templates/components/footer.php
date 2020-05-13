db9daee1f5736613219d937632d9abe3f5b0fead
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-13 21:28:30 +0200

    Translated footer

--- server/templates/components/footer.php
+++ server/templates/components/footer.php
@@ -1,6 +1,7 @@
 <footer class="footer py-3 bg-light d-none d-sm-block">
     <div class="container text-align-right">
-        <span class="text-dark">- Developed by <a class="font-weight-bold text-dark" href="/daw/profile/jofaval2/">Pepe
+        <span class="text-dark">- Desarrollado por <a class="font-weight-bold text-dark"
+                href="/daw/profile/jofaval2/">Pepe
                 Fabra Valverde</a>
             &copy; <span id="currentYear"></span>
             -</span>
