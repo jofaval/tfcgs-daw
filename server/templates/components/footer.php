40ec304b7f4bb6d6036bbb5d6546ebc14f09f3e7
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-14 01:19:22 +0200

    Fixed footer color

--- server/templates/components/footer.php
+++ server/templates/components/footer.php
@@ -1,7 +1,6 @@
 <footer class="footer py-3 bg-light d-none d-sm-block">
     <div class="container text-align-right">
-        <span class="text-dark">- Desarrollado por <a class="font-weight-bold text-dark"
-                href="/daw/profile/jofaval2/">Pepe
+        <span class="">- Desarrollado por <a class="font-weight-bold text-dark" href="/daw/profile/jofaval2/">Pepe
                 Fabra Valverde</a>
             &copy; <span id="currentYear"></span>
             -</span>
