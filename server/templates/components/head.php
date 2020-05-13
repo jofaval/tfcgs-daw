fdedd3edbc38c4b811f1b13e5fd4117e14150ae6
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-13 17:03:23 +0200

    Added apple capable meta

--- server/templates/components/head.php
+++ server/templates/components/head.php
@@ -9,6 +9,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <style>
     /*Avoid flashing the user with white background*/
     html,
