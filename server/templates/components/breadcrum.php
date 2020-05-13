a69d925e04ba65dff80b7314fea98a0c99df4329
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-13 16:21:57 +0200

    Added breadcrum icon sizing for responsiveness

--- server/templates/components/breadcrum.php
+++ server/templates/components/breadcrum.php
@@ -3,7 +3,8 @@
         <?php foreach ($breadcrumb as $elem): ?>
         <li class="breadcrumb-item">
             <a class="<?php echo $elem["active"] ? "" : "text-muted"; ?>" href="<?php echo $elem["link"]; ?>">
-                <span><i class="fa fa-<?php echo $elem["icon"]; ?>"></i></span>
+                <span class="d--smnone"><i class="fa fa-lg fa-<?php echo $elem["icon"]; ?>"></i></span>
+                <span class="d-none d-sm-inline"><i class="fa fa-<?php echo $elem["icon"]; ?>"></i></span>
                 <span class="text-overflow-ellipsis d-none d-sm-inline max-text-10"><?php echo $elem["name"]; ?></span>
             </a>
         </li>
