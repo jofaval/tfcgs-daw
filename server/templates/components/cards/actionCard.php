da46a321d434841e19550b6ef5b31bed692ac4e2
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-13 14:50:52 +0200

    Added actionCard with responsiveness

---
+++ server/templates/components/cards/actionCard.php
@@ -0,0 +1,23 @@
+<a href="<?php echo $action["link"]; ?>" title="<?php /* echo $action["name"]; */?>"
+    class="action d-none d-sm-block view overlay zoom cursor-pointer z-depth-1-half hoverable <?php echo $action["color"]; ?> m-2"
+    id="action<?php echo $action["id"]; ?>">
+    <div class="actionIcon img-fluid waves-light h-100 center-elements text-white p-3">
+        <i class="fa fa-<?php echo $action["icon"]; ?>"></i>
+    </div>
+    <div
+        class="actionTitle mask flex-center p-2 .z-depth-1 text-center d-flex justify-content-center text-white bg-dark">
+        <p class="align-self-center white-text fixed-line-spacing mb-0">
+            <?php echo $action["name"]; ?></p>
+    </div>
+</a>
+<a href="<?php echo $action["link"]; ?>" title="<?php /* echo $action["name"]; */?>"
+    class="action d-sm-none w-100 cursor-pointer px-0 shadow-none hoverable <?php echo $action["color"]; ?> m-2"
+    id="action<?php echo $action["id"]; ?>">
+    <div class="actionIcon img-fluid h-75 waves-light center-elements text-white p-3">
+        <i class="fa fa-<?php echo $action["icon"]; ?>"></i>
+    </div>
+    <div class="actionTitle h-25 w-100 .z-depth-1 text-center d-flex justify-content-center text-white bg-dark">
+        <p class="align-self-center white-text fixed-line-spacing mb-0">
+            <?php echo $action["name"]; ?></p>
+    </div>
+</a>
--- server/templates/project/overview.php
+++ server/templates/project/overview.php
@@ -36,30 +36,7 @@ $modelInstance = Model::getInstance();
                     <?php $actionGroup = $actionGroupDetails["actions"];?>
                     <?php foreach ($actionGroup as $action): ?>
                     <?php if ($userAccessLevel >= $action["access"]): ?>
-                    <a href="<?php echo $action["link"]; ?>" title="<?php /* echo $action["name"]; */?>"
-                        class="action d-none d-sm-block view overlay zoom cursor-pointer z-depth-1-half hoverable <?php echo $action["color"]; ?> m-2"
-                        id="action<?php echo $action["id"]; ?>">
-                        <div class="actionIcon img-fluid waves-light h-100 center-elements text-white p-3">
-                            <i class="fa fa-<?php echo $action["icon"]; ?>"></i>
-                        </div>
-                        <div
-                            class="actionTitle mask flex-center p-2 .z-depth-1 text-center d-flex justify-content-center text-white bg-dark">
-                            <p class="align-self-center white-text fixed-line-spacing mb-0">
-                                <?php echo $action["name"]; ?></p>
-                        </div>
-                    </a>
-                    <a href="<?php echo $action["link"]; ?>" title="<?php /* echo $action["name"]; */?>"
-                        class="action d-sm-none cursor-pointer col-5 px-0 z-depth-1-half hoverable <?php echo $action["color"]; ?> m-2"
-                        id="action<?php echo $action["id"]; ?>">
-                        <div class="actionIcon img-fluid h-75 waves-light center-elements text-white p-3">
-                            <i class="fa fa-<?php echo $action["icon"]; ?>"></i>
-                        </div>
-                        <div
-                            class="actionTitle h-25 w-100 .z-depth-1 text-center d-flex justify-content-center text-white bg-dark">
-                            <p class="align-self-center white-text fixed-line-spacing mb-0">
-                                <?php echo $action["name"]; ?></p>
-                        </div>
-                    </a>
+                    <?php include __DIR__ . "/../components/cards/actionCard.php";?>
                     <?php endif;?>
                     <?php endforeach;?>
                 </div>
