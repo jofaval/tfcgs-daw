abfb3c8eff95aba3f7501b9e572ef6f9a8d47c5a
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-14 14:07:42 +0200

    Added project/projects basic nightMode

--- client/scripts/js/nightMode.js
+++ client/scripts/js/nightMode.js
@@ -27,6 +27,10 @@ function changeNightMode() {
         $(".footer")
             .addClass("bg-transparent").removeClass("bg-light").addClass("text-white").removeClass("text-dark")
             .find("a").removeClass("text-dark").addClass("text-white");
+        $(".tabContentContainer").removeClass("bg-light")
+            .find(".bg-white").removeClass("bg-white").addClass("bg-dark");
+        $(".tabContentContainer *").addClass("text-white").removeClass("text-dark");
+        $(".pagination *").removeClass("text-dark");
     } else { //claro
         $("nav.navbar")
             .addClass("bg-light").removeClass("bg-transparent").removeClass("navbar-dark").addClass("navbar-light")
@@ -37,6 +41,10 @@ function changeNightMode() {
         $(".footer")
             .addClass("bg-light").removeClass("bg-transparent").removeClass("text-white").addClass("text-dark")
             .find("a").removeClass("text-white").addClass("text-dark");
+        $(".tabContentContainer").addClass("bg-light")
+            .find(".bg-dark").removeClass("bg-dark").addClass("bg-white");
+        $(".tabContentContainer *").addClass("text-dark").removeClass("text-white");
+        $(".pagination *").addClass("text-dark");
     }
 
     if (courtainEffect) {
