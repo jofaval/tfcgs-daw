b0217a4357c56d082243fa4931326d6c51093785
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-14 02:20:23 +0200

    Changed navbar responsive icon color

--- client/scripts/js/nightMode.js
+++ client/scripts/js/nightMode.js
@@ -17,7 +17,7 @@ function changeNightMode() {
 
     if (nightModeStatus) { //oscuro
         $("nav.navbar")
-            .removeClass("bg-light").addClass("bg-transparent")
+            .removeClass("bg-light").addClass("bg-transparent").removeClass("navbar-light").addClass("navbar-dark")
             .find(".nav-link:not(.btn)").removeClass("text-dark").addClass("text-white");
         $(".breadcrumb")
             .addClass("bg-transparent")
@@ -27,7 +27,7 @@ function changeNightMode() {
             .find("a").removeClass("text-dark").addClass("text-white");
     } else { //claro
         $("nav.navbar")
-            .addClass("bg-light").removeClass("bg-transparent")
+            .addClass("bg-light").removeClass("bg-transparent").removeClass("navbar-dark").addClass("navbar-light")
             .find(".nav-link:not(.btn)").addClass("text-dark").removeClass("text-white");
         $(".breadcrumb")
             .removeClass("bg-transparent")
