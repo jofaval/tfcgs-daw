49d94bed1405745452309db2b7d3981c4445eff3
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-14 03:17:59 +0200

    Changed dataTable rows

--- client/scripts/js/getDataTable.js
+++ client/scripts/js/getDataTable.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
     $('#dtBasicExample').DataTable();
+    var selectNumberofRows = $("select[name='dtBasicExample_length']");
+    selectNumberofRows.html("");
+    selectNumberofRows.append($(`<option value="5" selected="true">5</option>`))
+        .trigger("change");
+    for (let index = 2; index <= 5; index++) {
+        selectNumberofRows.append($(`<option value="${5 * index}">${5 * index}</option>`))
+            .trigger("change");
+        //crear array con numeracion para los options
+        //header order nocturno al principio
+    }
     $('.dataTables_length').addClass('bs-select');
 });
