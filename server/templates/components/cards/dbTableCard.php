6ff20557700cbb8a12204ef573cfabaa78a38fdc
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-14 16:52:01 +0200

    Fixed duplicated "Card" in dbTableCard

--- server/templates/components/cards/dbTableCard.php
+++ server/templates/components/cards/dbTableCard.php
@@ -1,9 +1,9 @@
 <a href="/daw/admin/get-data-from-table/<?php echo $tableName ?>/"
-    class="dbTableCardCard text-dark row col-12 bg-dark p-5 center-elements m-2 col-sm bg-grey">
+    class="dbTableCard text-dark row col-12 bg-dark p-5 center-elements m-2 col-sm bg-grey">
     <img src="https://picsum.photos/400/400?random=<?php echo $randomIndex ?>" alt=""
-        class="dbTableCardCardBgImg object-fit-cover opacity-30 position-absolute w-100 h-100 z-index">
-    <div class="row dbTableCardCardDetails pl-3 z-index-overlap flex-wrap center-elements w-100 m-0 pt-2">
-        <h5 class="dbTableCardCardTitle text-white m-0 font-weight-bold">
+        class="dbTableCardBgImg object-fit-cover opacity-30 position-absolute w-100 h-100 z-index">
+    <div class="row dbTableCardDetails pl-3 z-index-overlap flex-wrap center-elements w-100 m-0 pt-2">
+        <h5 class="dbTableCardTitle text-white m-0 font-weight-bold">
             <?php echo $tableName ?></h5>
     </div>
 </a>
