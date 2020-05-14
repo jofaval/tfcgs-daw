77436fe33504d96ee383393f1eddfc2bbcec2a43
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-14 13:55:34 +0200

    Changed footer project color for nightmode

--- server/templates/components/footerProject.php
+++ server/templates/components/footerProject.php
@@ -1,8 +1,8 @@
-<footer class="footer py-3 bg-light z-index-overlap d-none d-sm-block">
+<footer class="footer py-3 bg-light text-dark z-index-overlap d-none d-sm-block">
     <div class="container text-align-right">
         <span class="">Creador por <a href="/daw/profile/<?php echo $projectData["projectCreatorUsername"]; ?>/"
                 class="projectCreatedBy text-dark font-weight-bold"><?php echo $projectData["projectCreator"]; ?></a>
             el <span
-                class="projectCreationDate text-dark font-weight-bold"><?php echo date("d-m-Y", strtotime($projectData["projectCreationDate"])); ?></span>
+                class="projectCreationDate font-weight-bold"><?php echo date("d-m-Y", strtotime($projectData["projectCreationDate"])); ?></span>
     </div>
 </footer>
