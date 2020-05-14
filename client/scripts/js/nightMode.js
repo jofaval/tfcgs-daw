255bfdbb4f133f680838248aaa0ec3966877b0fa
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-14 14:21:08 +0200

    Added tabs and footer nightMode change

--- client/scripts/js/nightMode.js
+++ client/scripts/js/nightMode.js
@@ -27,10 +27,20 @@ function changeNightMode() {
         $(".footer")
             .addClass("bg-transparent").removeClass("bg-light").addClass("text-white").removeClass("text-dark")
             .find("a").removeClass("text-dark").addClass("text-white");
+
         $(".tabContentContainer").removeClass("bg-light")
             .find(".bg-white").removeClass("bg-white").addClass("bg-dark");
-        $(".tabContentContainer *").addClass("text-white").removeClass("text-dark");
+        $(".tabContentContainer *:not(.dashboardCard)").addClass("text-white").removeClass("text-dark");
+
+        $(".tabs").removeClass("bg-light")
+            .find(".bg-white").removeClass("bg-white").addClass("bg-dark");
+        $(".tabs *").addClass("text-white").removeClass("text-dark");
+
+        $(".footer, .tabs").removeClass("bg-light grey lighten-4").addClass("bg-dark");
+
         $(".pagination *").removeClass("text-dark");
+
+        THEME = DARK_THEME;
     } else { //claro
         $("nav.navbar")
             .addClass("bg-light").removeClass("bg-transparent").removeClass("navbar-dark").addClass("navbar-light")
@@ -41,10 +51,20 @@ function changeNightMode() {
         $(".footer")
             .addClass("bg-light").removeClass("bg-transparent").removeClass("text-white").addClass("text-dark")
             .find("a").removeClass("text-white").addClass("text-dark");
+
         $(".tabContentContainer").addClass("bg-light")
             .find(".bg-dark").removeClass("bg-dark").addClass("bg-white");
-        $(".tabContentContainer *").addClass("text-dark").removeClass("text-white");
+        $(".tabContentContainer *:not(.dashboardCard)").addClass("text-dark").removeClass("text-white");
+
+        $(".tabs").addClass("bg-light")
+            .find(".bg-dark").removeClass("bg-dark").addClass("bg-white");
+        $(".tabs *").addClass("text-dark").removeClass("text-white");
+
+        $(".footer, .tabs").removeClass("bg-light bg-dark").addClass("grey lighten-4");
+
         $(".pagination *").addClass("text-dark");
+
+        THEME = MIXED_THEME;
     }
 
     if (courtainEffect) {
