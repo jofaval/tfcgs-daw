55846467037deef6a29ab5040533550581cafa85
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-15 22:24:25 +0200

    Changed database configuration

--- dist/server/classes/config/database.php
+++ dist/server/classes/config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-Config::$mvc_bd_hostname = "localhost";
-Config::$mvc_bd_nombre = "origen";
-Config::$mvc_bd_usuario = "test";
-Config::$mvc_bd_clave = "";
+Config::$mvc_bd_hostname = "sql101.epizy.com";
+Config::$mvc_bd_nombre = "epiz_25783103_origen";
+Config::$mvc_bd_usuario = "epiz_25783103";
+Config::$mvc_bd_clave = "C4fJuiphIl";
