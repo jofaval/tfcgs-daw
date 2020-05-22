26aef988daa2d4216c9e08a310945a95912f3366
pepe_fabra_valverde <jofaval@outlook.com>
2020-05-23 00:59:25 +0200

    Moved profileNotFound template inside profile folder

---
+++ src/server/templates/profile/profileNotFound.php
@@ -0,0 +1,43 @@
+<!--Page configuration-->
+<?php $optionalCSS = ["style.css"];?>
+<?php $optionalScripts = ["js/script.js"];?>
+<?php $title = "Example";?>
+<?php $showFooter = true;?>
+<?php $showHeader = true;?>
+<?php $showBreadcrumb = true;?>
+<?php
+$breadcrumb = [
+    [
+        "name" => "Home",
+        "link" => Config::$EXECUTION_HOME_PATH . "",
+        "active" => false,
+        "icon" => "home",
+    ],
+    [
+        "name" => "Perfil",
+        "link" => Config::$EXECUTION_HOME_PATH . "profile/",
+        "active" => false,
+        "icon" => "user",
+    ],
+    [
+        "name" => "Perfil no encontrado",
+        "link" => Config::$EXECUTION_HOME_PATH . "profile/not-found/",
+        "active" => true,
+        "icon" => "exclamation-triangle",
+    ],
+];
+?>
+
+<?php ob_start()?>
+
+<div class="m-auto container text-center">
+    <span>
+        <i class="fa h1 fa-user-times" style="font-size: 5rem !important;"></i>
+    </span>
+    <br>
+    <h3>No hemos podido encontrar este perfil, lo sentimos</h3>
+</div>
+
+<?php $contenido = ob_get_clean()?>
+
+<?php include_once __DIR__ . '/../layout.php'?>
