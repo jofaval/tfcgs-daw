a24139f9c1122480f7b007204dbbc67c6e0e794c
pepe_fabra_valverde <jofaval@outlook.com>
2020-06-07 01:45:24 +0200

    Added summernote editor to layout editor

---
+++ src/client/scripts/js/admin/layoutEditor.js
@@ -0,0 +1,28 @@
+var summernote = $('#summernote').summernote({
+    lineHeights: ['0.2', '0.3', '0.4', '0.5', '0.6', '0.8', '1.0', '1.2', '1.4', '1.5', '2.0', '3.0'],
+    disableDragAndDrop: true,
+    height: 400,
+    codeviewFilter: false,
+    codeviewIframeFilter: true,
+    tabDisable: true,
+    placeholder: '',
+    enable: false,
+    codemirror: { // codemirror options
+        theme: 'monokai'
+    },
+    toolbar: [
+        ['style', ['style']],
+        ['font', ['bold', 'underline', 'clear']],
+        ['fontname', ['fontname', 'fontsize', 'fontsizeunit']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture', 'video']],
+        ['view', ['fullscreen', 'codeview', 'help']],
+    ],
+    oninit: function () {
+        console.log("test");
+    }
+});
+
+$("div.note-editor button[data-event='codeview']").click();
--- src/server/templates/admin/layoutEditor.php
+++ src/server/templates/admin/layoutEditor.php
@@ -1,10 +1,11 @@
 <!--Page configuration-->
-<?php $optionalCSS = ["style.css"];?>
-<?php $optionalScripts = ["js/script.js"];?>
-<?php $title = "Example";?>
+<?php $optionalCSS = ["summernote-bs4.min.css"];?>
+<?php $optionalScripts = ["libs/summernote-bs4.min.js", "js/admin/layoutEditor.js"];?>
+<?php $title = "Editor de layouts";?>
 <?php $showFooter = true;?>
 <?php $showHeader = true;?>
 <?php $showBreadcrumb = true;?>
+
 <?php
 $breadcrumb = [
     [
@@ -14,10 +15,16 @@ $breadcrumb = [
         "icon" => "home",
     ],
     [
-        "name" => "Example",
-        "link" => Config::$EXECUTION_HOME_PATH . "example/",
+        "name" => "Admin",
+        "link" => Config::$EXECUTION_HOME_PATH . "admin/",
         "active" => true,
-        "icon" => "home",
+        "icon" => "cogs",
+    ],
+    [
+        "name" => "Editor de layouts",
+        "link" => Config::$EXECUTION_HOME_PATH . "admin/layoutEditor/",
+        "active" => true,
+        "icon" => "edit",
     ],
 ];
 ?>
@@ -25,7 +32,16 @@ $breadcrumb = [
 <?php ob_start()?>
 
 <div class="m-auto bg-dark p-5 rounded">
-
+    <form action="" method="POST" class="text-center">
+        <select class="browser-default custom-select" name="templateName" id="templateName">
+            <?php foreach ($viewParams["files"] as $file): ?>
+            <option value=""><?php echo substr($file, strpos($file, "templates") + strlen("templates") + 1); ?></option>
+            <?php endforeach;?>
+        </select>
+        <input type="submit" class="btn btn-primary m-3" value="Cargar contenido del fichero" name="loadFileContent"
+            id="loadFileContent">
+    </form>
+    <div id="summernote"></div>
 </div>
 
 <?php $contenido = ob_get_clean()?>
