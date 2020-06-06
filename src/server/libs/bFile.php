58f8f3d893752e5610cecc2699e3f566fb5af1b6
pepe_fabra_valverde <jofaval@outlook.com>
2020-06-07 01:45:13 +0200

    Add get all files from dir

--- src/server/libs/bFile.php
+++ src/server/libs/bFile.php
@@ -80,4 +80,21 @@ class FileUtils
     {
         copy($source, $target);
     }
+
+    public static function getDirContents($dir, &$results = array())
+    {
+        $files = scandir($dir);
+
+        foreach ($files as $key => $value) {
+            $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+            if (!is_dir($path)) {
+                $results[] = $path;
+            } else if ($value != "." && $value != "..") {
+                FileUtils::getDirContents($path, $results);
+                $results[] = $path;
+            }
+        }
+
+        return $results;
+    }
 }
