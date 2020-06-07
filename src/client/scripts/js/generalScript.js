3b5fc37ea2fb11edd5dd7fc731008ef026f8f485
pepe_fabra_valverde <jofaval@outlook.com>
2020-06-07 14:33:49 +0200

    Fixed dropdown toggle bug

--- src/client/scripts/js/generalScript.js
+++ src/client/scripts/js/generalScript.js
@@ -45,9 +45,9 @@ $(window).on("load", function () {
         $("main, body").scrollTop(0);
     });
 
-    $("#signinFormBtnDropdown").click();
+    /* $("#signinFormBtnDropdown").click(); */
     $("input").focus().blur();
-    $(".dropdown-toggle").click();
+    //$(".dropdown-toggle").click();
 
     vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
